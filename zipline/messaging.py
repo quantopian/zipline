8e840e472ebcd2c3a3d5fcb829fa6dce02ffa313
fawce <fawce@quantopian.com>
2012-02-23 08:11:13 -0500

    cleaning local changes

--- zipline/messaging.py
+++ zipline/messaging.py
@@ -108,12 +108,6 @@ class Component(object):
     def connect_data(self):
         return self.connect_push_socket(self.addresses['data_address'])
         
-    def bind_order(self):
-        return self.bind_pull_socket(self.addresses['order_address'])
-
-    def connect_order(self):
-        return self.connect_push_socket(self.addresses['order_address'])
-        
     def bind_feed(self):
         return self.bind_pub_socket(self.addresses['feed_address'])
     
@@ -482,35 +476,4 @@ class DataSource(Component):
     def get_type(self):
         raise NotImplemented
 
-
-class Client(Component):
-
-    def __init__(self):
-        Component.__init__(self)
-        self.received_count     = 0
-        self.prev_dt            = None
-
-    def open(self):
-        self.data_feed, self.poller = self.connect_result()
-        self.order_feed, self.poller = self.connect_order()
-
-    def do_work(self):
-        socks = dict(self.poller.poll(2000)) #timeout after 2 seconds.
-        if self.data_feed in socks and socks[self.data_feed] == self.zmq.POLLIN:   
-            msg = self.data_feed.recv()
-            if(self.is_done_message(msg)):
-                qutil.LOGGER.info("Client is DONE!")
-                self.signal_done()
-                return
-
-            self.received_count += 1
-            event = json.loads(msg)
-            self.handle_event(event)
-            
-    def handle_event(self, event):
-        NotImplemented
-        
-    
-    def order(self, sid, volume):
-        order = {'sid':sid, 'volume':volume}
     
