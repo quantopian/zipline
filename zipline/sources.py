e8567d9305baebb65ddec2cc2e26da70528082b6
fawce <fawce@quantopian.com>
2012-02-26 20:06:17 -0500

    adding new finance test cases and a factories module to support them

--- zipline/sources.py
+++ zipline/sources.py
@@ -9,7 +9,7 @@ import zipline.messaging as qmsg
 
 class RandomEquityTrades(qmsg.DataSource):
     """Generates a random stream of trades for testing."""
-
+    
     def __init__(self, sid, source_id, count):
         qmsg.DataSource.__init__(self, source_id)
         self.count          = count
@@ -18,14 +18,17 @@ class RandomEquityTrades(qmsg.DataSource):
         self.trade_start    = datetime.datetime.now()
         self.minute         = datetime.timedelta(minutes=1)
         self.price          = random.uniform(5.0, 50.0)
-
+    
+    
     def get_type(self):
         return 'equity_trade'
-
+    
+    
     def do_work(self):
         if(self.incr == self.count):
             self.signal_done()
             return
+        
         self.price = self.price + random.uniform(-0.05, 0.05)
         event = {
             'sid'    : self.sid,
@@ -33,6 +36,37 @@ class RandomEquityTrades(qmsg.DataSource):
             'price'  : self.price,
             'volume' : random.randrange(100,10000,100)
         }
-
+        
         self.send(event)
         self.incr += 1
+    
+
+
+class SpecificEquityTrades(qmsg.DataSource):
+    """Generates a random stream of trades for testing."""
+
+    def __init__(self, source_id, event_list):
+        """
+        :event_list: should be a chronologically ordered list of dictionaries in the following form:
+                event = {
+                    'sid'    : self.sid,
+                    'dt'     : qutil.format_date(self.trade_start + (self.minute * self.incr)),
+                    'price'  : self.price,
+                    'volume' : random.randrange(100,10000,100)
+                }
+        """
+        qmsg.DataSource.__init__(self, source_id)
+        self.event_list = event_list
+
+    def get_type(self):
+        return 'equity_trade'
+
+    def do_work(self):
+        if(len(self.event_list) == 0):
+            self.signal_done()
+            return
+        
+        event = self.event_list.pop(0)
+        self.send(event)
+        
+
---
+++ zipline/test/test_finance.py
@@ -0,0 +1,85 @@
+"""Tests for the zipline.finance package"""
+from unittest2 import TestCase
+
+from zipline.test.test_messaging import SimulatorTestCase
+from zipline.monitor import Controller
+from zipline.messaging import DataSource
+import zipline.util as qutil
+
+class ThreadPoolExecutor(SimulatorTestCase, TestCase):
+
+    allocator = DummyAllocator(100)
+
+    def setup_logging(self):
+        qutil.configure_logging()
+
+        # lazy import by design
+        self.logger = mock.Mock()
+
+    def setup_allocator(self):
+        pass
+
+    def get_simulator(self, addresses):
+        return ThreadSimulator(addresses)
+
+    def get_controller(self):
+        # Allocate two more sockets
+        controller_sockets = self.allocate_sockets(2)
+
+        return Controller(
+            controller_sockets[0],
+            controller_sockets[1],
+            logging = self.logger,
+        )
+
+    #
+    def test_orders(self):
+
+        # Base Simuation
+        # --------------
+
+        # Allocate sockets for the simulator components
+        sockets = self.allocate_sockets(6)
+
+        addresses = {
+            'sync_address'   : sockets[0],
+            'data_address'   : sockets[1],
+            'feed_address'   : sockets[2],
+            'merge_address'  : sockets[3],
+            'result_address' : sockets[4],
+            'order_address'  : sockets[5]
+        }
+
+        sim = self.get_simulator(addresses)
+        con = self.get_controller()
+
+        # Simulation Components
+        # ---------------------
+
+        ret1 = RandomEquityTrades(133, "ret1", 5000)
+        ret2 = RandomEquityTrades(134, "ret2", 5000)
+        mavg1 = MovingAverage("mavg1", 30)
+        mavg2 = MovingAverage("mavg2", 60)
+        client = TestClient(self, expected_msg_count=10000)
+
+        sim.register_components([ret1, ret2, mavg1, mavg2, client])
+        sim.register_controller( con )
+
+        # Simulation
+        # ----------
+        sim.simulate()
+
+        # Stop Running
+        # ------------
+
+        # TODO: less abrupt later, just shove a StopIteration
+        # down the pipe to make it stop spinning
+        sim.cuc._Thread__stop()
+
+        self.assertEqual(sim.feed.pending_messages(), 0,
+            "The feed should be drained of all messages, found {n} remaining."
+            .format(n=sim.feed.pending_messages())
+        )
+        
+
+class PredefinedDataSource()
