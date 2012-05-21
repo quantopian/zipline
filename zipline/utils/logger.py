46ef2e136d3b60da8ecedb7e2708e06a4f8f8880
fawce <fawce@quantopian.com>
2012-05-21 16:21:29 -0400

    path hacking for logging.cfg

--- zipline/utils/logger.py
+++ zipline/utils/logger.py
@@ -5,9 +5,15 @@ and other common operations.
 
 import logging
 import logging.config
+from os.path import join, abspath, dirname
 
 def configure_logging():
     logging.config.fileConfig(
-        'logging.cfg',
+        logger_path(),
         disable_existing_loggers = False
     )
+
+def logger_path():
+    import zipline
+    log_path = dirname(abspath(zipline.__file__))
+    return join(log_path, 'logging.cfg')
