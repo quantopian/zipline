da827c18b48c0dab553f76343b304e8d4b38059c
Thomas Wiecki <thomas.wiecki@gmail.com>
2012-05-30 10:15:35 -0400

    Fixed unittest bug (not copying config dict). Further refactoring of optimization tests and factory. Added documentation to optimization factory functions.

--- zipline/optimize/factory.py
+++ zipline/optimize/factory.py
@@ -3,7 +3,7 @@ Factory functions to prepare useful data for optimize tests.
 
 Author: Thomas V. Wiecki (thomas.wiecki@gmail.com), 2012
 """
-from datetime import datetime, timedelta
+from datetime import timedelta
 
 import zipline.protocol as zp
 
@@ -13,13 +13,34 @@ from zipline.optimize.algorithms import BuySellAlgorithm
 from zipline.lines import SimulatedTrading
 from zipline.finance.trading import SIMULATION_STYLE
 
-from copy import deepcopy
+from copy import copy
 from itertools import cycle
 
-def create_updown_trade_source(sid, trade_count, trading_environment, start_price, amplitude):
+def create_updown_trade_source(sid, trade_count, trading_environment, base_price, amplitude):
+    """Create the updown trade source. This source emits events with
+    the price going up and down by the same amount in each
+    iteration. The trade source is thus perfectly predictable. This is
+    used for a test case for the optimization code.
+
+    :Arguments:
+        sid : int
+            SID of stock to create.
+        trade_count : int
+            How many trade events to create (will also influence order count)
+        trading_environment : TradeEnvironment object
+            The trading environment to use (see zipline.factory.create_trading_environment)
+        base_price : int
+            The average price that each iteration will hover around.
+        amplitude : int
+            How much the price will go up and down each iteration.
+
+    :Returns:
+        source : SpecificEquityTrades
+            The trade source emitting up down events.
+    """
     volume = 1000
     events = []
-    price = start_price-amplitude/2.
+    price = base_price-amplitude/2.
 
     cur = trading_environment.first_open
     one_day = timedelta(days = 1)
@@ -49,8 +70,49 @@ def create_updown_trade_source(sid, trade_count, trading_environment, start_pric
     return source
 
 
-def create_predictable_zipline(config, sid=133, amplitude=10, base_price=50, offset=0, trade_count=3, simulate=True):
-    #config = deepcopy(config)
+def create_predictable_zipline(config, offset=0, simulate=True):
+    """Create a test zipline object as specified by config. The
+    zipline will use the UpDown tradesource which is perfectly
+    predictable.
+
+    Trade source parameters can be specified inside the config object.
+
+    :Trade source arguments:
+        config['sid'] : int
+            SID of stock to create.
+        config['amplitude'] : int (default 10)
+            How much the price will go up and down each iteration.
+        config['base_price'] : int (default 50)
+            The average price that each iteration will hover around.
+        config['trade_count'] : int (default 3)
+            How many trade events to create (will also influence order count)
+
+    If not specified, the BuySellAlgorithm is used by default. This
+    can be changed by setting config['algorithm'].
+
+    :Arguments:
+        offset : int (default 0)
+            The offset parameter specifies how much the BuySellAlgorithm will
+            order each iteration and is a negative quadratic centered around
+            0. Thus, any deviations from 0 will lead to less buy and sell
+            orders each iteration and ultimately to less compound returns.
+        simulate : bool (default True)
+            Whether to call .simulate(blocking=True) on the created zipline
+            argument.
+
+    :Returns:
+        zipline : class zipline
+            created zipline object
+        config : dict
+            the config dict used to create the zipline
+    """
+    config = copy(config)
+    sid = config['sid']
+    # remove
+    amplitude = config.pop('amplitude', 10)
+    base_price = config.pop('base_price', 50)
+    trade_count = config.pop('trade_count', 3)
+
     trading_environment = create_trading_environment()
     source = create_updown_trade_source(sid,
                                         trade_count,
