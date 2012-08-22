f8fd508dfc5ef9e89816b71bb39bcfda32e910a9
Eddie Hebert <ehebert@quantopian.com>
2012-08-22 13:38:14 -0400

    Removes custom Exception that isn't found in code base.

--- zipline/exceptions.py
+++
@@ -1,5 +0,0 @@
-from utils.exception_utils import CustomException
-
-class ComponentNoInit(CustomException):
-    argmap  = ('classname',)
-    message = """Class {classname} does not define an init method."""
