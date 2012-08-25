1f5cb86940a7586f50b3dcc1a65704e0246427e1
Eddie Hebert <eddie@eddiehebert.com>
2012-08-25 01:34:19 -0400

    Applies PEP-8 recommendations.
    Whitespace wrangling.

--- tests/test_monitor.py
+++ tests/test_monitor.py
@@ -3,16 +3,16 @@ from unittest2 import TestCase, skip
 
 from zipline.core.monitor import Monitor
 
+
 class TestMonitor(TestCase):
     def setUp(self):
         setup_logger(self, '/var/log/qexec/qexec.log')
-
 
     def tearDown(self):
         teardown_logger(self)
 
     def test_init(self):
-        pub_socket   = 'tcp://127.0.0.1:5000'
+        pub_socket = 'tcp://127.0.0.1:5000'
         route_socket = 'tcp://127.0.0.1:5001'
         exception_socket = 'tcp://127.0.0.1:5002'
 
@@ -20,9 +20,9 @@ class TestMonitor(TestCase):
         mon.manage([])
 
     def test_init_topology(self):
-        pub_socket   = 'tcp://127.0.0.1:5000'
+        pub_socket = 'tcp://127.0.0.1:5000'
         route_socket = 'tcp://127.0.0.1:5001'
         exception_socket = 'tcp://127.0.0.1:5002'
 
         mon = Monitor(pub_socket, route_socket, exception_socket)
-        mon.manage([ 'a', 'b', 'c', 'd' ])
+        mon.manage(['a', 'b', 'c', 'd'])
