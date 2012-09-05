48476cb7a754bba47dc2d284befede6abf2286a4
Eddie Hebert <eddie@eddiehebert.com>
2012-09-05 08:08:05 -0400

    Removes noop test.

--- tests/test_sanity.py
+++
@@ -1,7 +0,0 @@
-from unittest2 import TestCase
-
-class TestEnviroment(TestCase):
-
-    def test_universe(self):
-        # first order logic is working today. Yay!
-        self.assertTrue(True != False)
