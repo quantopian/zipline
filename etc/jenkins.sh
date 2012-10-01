44ba87ef01965c52a74528e713dcda28fe74fb87
fawce <fawce@quantopian.com>
2012-09-30 23:40:13 -0400

    cleaned up scientific install

--- etc/jenkins.sh
+++ etc/jenkins.sh
@@ -8,19 +8,10 @@ if [ ! -d $WORKON_HOME ]; then
 fi
 source /usr/local/bin/virtualenvwrapper.sh
 
-
-#create the scientific python virtualenv and copy to provide zipline base
-mkvirtualenv --no-site-packages scientific_base
-workon scientific_base
-./etc/ordered_pip.sh ./etc/requirements_sci.txt
-deactivate
-#re-base zipline
-#rmvirtualenv zipline
-cpvirtualenv scientific_base zipline  
-
 workon zipline
 ./etc/ordered_pip.sh ./etc/requirements.txt
 ./etc/ordered_pip.sh ./etc/requirements_dev.txt
+./etc/ordered_pip.sh ./etc/requirements_sci.txt
 
 # Show what we have installed
 pip freeze
