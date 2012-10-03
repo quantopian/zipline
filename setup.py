f8c3f10589f6de0c1f788ccf4db970c633b42792
Eddie Hebert <ehebert@quantopian.com>
2012-10-03 15:13:20 -0400

    Strips out use of pavement in generating our setup.py
    Working towards making the project distributable on PyPI.
    
    Looked at a lot of the projects that we want to emulate, and none
    use pavement.

--- setup.py
+++ setup.py
@@ -1,9 +1,38 @@
-import os
-from setuptools import setup, find_packages
+#!/usr/bin/env python
 
-if os.path.exists("paver-minilib.zip"):
-    import sys
-    sys.path.insert(0, "paver-minilib.zip")
+from distutils.core import setup
 
-import paver.tasks
-paver.tasks.main()
+setup(name='zipline',
+      version='0.1',
+      description='A backtester for financial algorithms.',
+      author='Quantopian Inc.',
+      author_email='opensource@quantopian.com',
+      packages=['zipline'],
+      long_description=open('README.md').read(),
+      license='BSD',
+      classifiers=[
+          'Development Status :: 2 - Pre-Alpha',
+          'License :: OSI Approved :: BSD License',
+          'Natural Language :: English',
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: C',
+          'Programming Language :: Cython',
+          'Operating System :: OS Independent',
+          'Intended Audience :: Science/Research',
+          'Topic :: Office/Business :: Financial',
+          'Topic :: Scientific/Engineering :: Information Analysis',
+          'Topic :: System :: Distributed Computing',
+      ],
+      install_requires=[
+          'msgpack-python',
+          'humanhash',
+          'iso8601',
+          'Logbook',
+          'blist',
+          'pytz',
+          'numpy',
+          'Cython',
+          'pandas'
+          ]
+)
