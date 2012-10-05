b6af686392db1bab906dbb4e635a03137677ed9a
Eddie Hebert <ehebert@quantopian.com>
2012-10-05 14:31:51 -0400

    Updates version to initial release version in setup.py

--- setup.py
+++ setup.py
@@ -3,21 +3,19 @@
 from distutils.core import setup
 
 setup(name='zipline',
-      version='0.1',
+      version='0.5.0',
       description='A backtester for financial algorithms.',
       author='Quantopian Inc.',
       author_email='opensource@quantopian.com',
       packages=['zipline'],
       long_description=open('README.md').read(),
-      license='BSD',
+      license='Apache 2.0',
       classifiers=[
           'Development Status :: 2 - Pre-Alpha',
-          'License :: OSI Approved :: BSD License',
+          'License :: OSI Approved :: Apache Software License',
           'Natural Language :: English',
           'Programming Language :: Python',
           'Programming Language :: Python :: 2.7',
-          'Programming Language :: C',
-          'Programming Language :: Cython',
           'Operating System :: OS Independent',
           'Intended Audience :: Science/Research',
           'Topic :: Office/Business :: Financial',
@@ -26,13 +24,11 @@ setup(name='zipline',
       ],
       install_requires=[
           'msgpack-python',
-          'humanhash',
           'iso8601',
           'Logbook',
           'blist',
           'pytz',
           'numpy',
-          'Cython',
           'pandas'
           ]
 )
