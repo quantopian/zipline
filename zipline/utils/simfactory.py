8bd6b675b2a7fcb2b5785a934ed4f593f4bdb963
fawce <fawce@quantopian.com>
2012-10-08 12:06:22 -0400

    fixed weird indentation bug

--- zipline/utils/simfactory.py
+++ zipline/utils/simfactory.py
@@ -37,78 +37,78 @@ def create_test_zipline(**config):
         sid = config.get('sid')
         sid_list = [sid]
 
-        concurrent_trades = config.get('concurrent_trades', False)
+    concurrent_trades = config.get('concurrent_trades', False)
 
-        #--------------------
-        # Trading Environment
-        #--------------------
-        if 'environment' in config:
-            trading_environment = config['environment']
-        else:
-            trading_environment = factory.create_trading_environment()
+    #--------------------
+    # Trading Environment
+    #--------------------
+    if 'environment' in config:
+        trading_environment = config['environment']
+    else:
+        trading_environment = factory.create_trading_environment()
 
-        if 'order_count' in config:
-            order_count = config['order_count']
-        else:
-            order_count = 100
+    if 'order_count' in config:
+        order_count = config['order_count']
+    else:
+        order_count = 100
 
-        if 'order_amount' in config:
-            order_amount = config['order_amount']
-        else:
-            order_amount = 100
+    if 'order_amount' in config:
+        order_amount = config['order_amount']
+    else:
+        order_amount = 100
 
-        if 'trade_count' in config:
-            trade_count = config['trade_count']
-        else:
-            # to ensure all orders are filled, we provide one more
-            # trade than order
-            trade_count = 101
+    if 'trade_count' in config:
+        trade_count = config['trade_count']
+    else:
+        # to ensure all orders are filled, we provide one more
+        # trade than order
+        trade_count = 101
 
-        slippage = config.get('slippage', FixedSlippage())
-        commission = PerShare()
-        transact_method = transact_partial(slippage, commission)
+    slippage = config.get('slippage', FixedSlippage())
+    commission = PerShare()
+    transact_method = transact_partial(slippage, commission)
 
-        #-------------------
-       # Trade Source
-        #-------------------
-        if 'trade_source' in config:
-            trade_source = config['trade_source']
-        else:
-            trade_source = factory.create_daily_trade_source(
-                sid_list,
-                trade_count,
-                trading_environment,
-                concurrent=concurrent_trades
-            )
-
-        #-------------------
-        # Transforms
-        #-------------------
-        transforms = config.get('transforms', [])
-
-        #-------------------
-        # Create the Algo
-        #-------------------
-        if 'algorithm' in config:
-            test_algo = config['algorithm']
-        else:
-            test_algo = TestAlgorithm(
-                sid,
-                order_amount,
-                order_count
-           )
-
-        #-------------------
-        # Simulation
-        #-------------------
-
-        sim = SimulatedTrading(
-            [trade_source],
-            transforms,
-            test_algo,
+    #-------------------
+   # Trade Source
+    #-------------------
+    if 'trade_source' in config:
+        trade_source = config['trade_source']
+    else:
+        trade_source = factory.create_daily_trade_source(
+            sid_list,
+            trade_count,
             trading_environment,
-            transact_method
+            concurrent=concurrent_trades
         )
-        #-------------------
 
-        return sim
+    #-------------------
+    # Transforms
+    #-------------------
+    transforms = config.get('transforms', [])
+
+    #-------------------
+    # Create the Algo
+    #-------------------
+    if 'algorithm' in config:
+        test_algo = config['algorithm']
+    else:
+        test_algo = TestAlgorithm(
+            sid,
+            order_amount,
+            order_count
+       )
+
+    #-------------------
+    # Simulation
+    #-------------------
+
+    sim = SimulatedTrading(
+        [trade_source],
+        transforms,
+        test_algo,
+        trading_environment,
+        transact_method
+    )
+    #-------------------
+
+    return sim
