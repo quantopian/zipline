c0b260fc4ada598eb6c6c6f4b7b6317fc42d3053
Eddie Hebert <eddie@eddiehebert.com>
2012-10-25 22:31:15 -0400

    Bumps version up to 0.5.1 in setup config.

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@
 from setuptools import setup, find_packages
 
 setup(name='zipline',
-      version='0.5.0',
+      version='0.5.1',
       description='A backtester for financial algorithms.',
       author='Quantopian Inc.',
       author_email='opensource@quantopian.com',
