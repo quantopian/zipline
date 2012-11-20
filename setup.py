7ff1ab2ab1e83bc59e6d35061ad00886fedaf90a
Eddie Hebert <ehebert@quantopian.com>
2012-11-20 16:08:52 -0500

    Makes indentation of setup.py compatible with flake8

--- setup.py
+++ setup.py
@@ -16,35 +16,36 @@
 
 from setuptools import setup, find_packages
 
-setup(name='zipline',
-      version='0.5.2',
-      description='A backtester for financial algorithms.',
-      author='Quantopian Inc.',
-      author_email='opensource@quantopian.com',
-      packages=find_packages(),
-      long_description=open('README.md').read(),
-      license='Apache 2.0',
-      classifiers=[
-          'Development Status :: 4 - Beta',
-          'License :: OSI Approved :: Apache Software License',
-          'Natural Language :: English',
-          'Programming Language :: Python',
-          'Programming Language :: Python :: 2.7',
-          'Operating System :: OS Independent',
-          'Intended Audience :: Science/Research',
-          'Topic :: Office/Business :: Financial',
-          'Topic :: Scientific/Engineering :: Information Analysis',
-          'Topic :: System :: Distributed Computing',
-      ],
-      install_requires=[
-          'msgpack-python',
-          'iso8601',
-          'Logbook',
-          'blist',
-          'pytz',
-          'requests',
-          'numpy',
-          'pandas'
-      ],
-      url="https://github.com/quantopian/zipline"
+setup(
+    name='zipline',
+    version='0.5.2',
+    description='A backtester for financial algorithms.',
+    author='Quantopian Inc.',
+    author_email='opensource@quantopian.com',
+    packages=find_packages(),
+    long_description=open('README.md').read(),
+    license='Apache 2.0',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: Apache Software License',
+        'Natural Language :: English',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Operating System :: OS Independent',
+        'Intended Audience :: Science/Research',
+        'Topic :: Office/Business :: Financial',
+        'Topic :: Scientific/Engineering :: Information Analysis',
+        'Topic :: System :: Distributed Computing',
+    ],
+    install_requires=[
+        'msgpack-python',
+        'iso8601',
+        'Logbook',
+        'blist',
+        'pytz',
+        'requests',
+        'numpy',
+        'pandas'
+    ],
+    url="https://github.com/quantopian/zipline"
 )
