0bb5be0a973dea70ebe9ebc41308d953fbe8d454
Ryan Day <ryanday2@gmail.com>
2013-02-18 13:27:19 -0500

    Update msgpack files if new data is available

--- zipline/data/loader.py
+++ zipline/data/loader.py
@@ -18,12 +18,13 @@ import os
 from os.path import expanduser
 import msgpack
 from collections import OrderedDict
-
+from datetime import timedelta
 
 from treasuries import get_treasury_data
 from benchmarks import get_benchmark_returns
 
 from zipline.utils.date_utils import tuple_to_date
+from zipline.utils.tradingcalendar import trading_days
 from operator import attrgetter
 
 
@@ -86,6 +87,58 @@ def dump_benchmarks(symbol):
         bmark_fp.write(msgpack.dumps(benchmark_data))
 
 
+def update_treasury_curves(last_date):
+    """
+    Updates data in the zipline treasury curves message pack
+
+    last_date should be a datetime object of the most recent data
+
+    Puts source treasury and data into zipline.
+    """
+    tr_data = []
+    with get_datafile('treasury_curves.msgpack', mode='rb') as tr_fp:
+        tr_list = msgpack.loads(tr_fp.read())
+        for packed_date, curve in tr_list:
+            tr_data.append((packed_date, curve))
+
+    for curve in get_treasury_data():
+        date_as_tuple = curve['date'].timetuple()[0:6] + \
+            (curve['date'].microsecond,)
+        # Not ideal but massaging data into expected format
+        del curve['date']
+        tr = (date_as_tuple, curve)
+        tr_data.append(tr)
+
+    with get_datafile('treasury_curves.msgpack', mode='wb') as tr_fp:
+        tr_fp.write(msgpack.dumps(tr_data))
+
+
+def update_benchmarks(symbol, last_date):
+    """
+    Updates data in the zipline message pack
+
+    last_date should be a datetime object of the most recent data
+
+    Puts source benchmark into zipline.
+    """
+    benchmark_data = []
+    with get_datafile(get_benchmark_filename(symbol), mode='rb') as bmark_fp:
+        bm_list = msgpack.loads(bmark_fp.read())
+        for packed_date, returns in bm_list:
+            benchmark_data.append((packed_date, returns))
+
+    start = last_date+timedelta(days=1)
+    for daily_return in get_benchmark_returns(symbol, start_date=start):
+        date_as_tuple = daily_return.date.timetuple()[0:6] + \
+            (daily_return.date.microsecond,)
+        # Not ideal but massaging data into expected format
+        benchmark = (date_as_tuple, daily_return.returns)
+        benchmark_data.append(benchmark)
+
+    with get_datafile(get_benchmark_filename(symbol), mode='wb') as bmark_fp:
+        bmark_fp.write(msgpack.dumps(benchmark_data))
+
+
 def get_benchmark_filename(symbol):
     return "%s_benchmark.msgpack" % symbol
 
@@ -104,6 +157,20 @@ Fetching data from Yahoo Finance.
         fp_bm = get_datafile(get_benchmark_filename(bm_symbol), "rb")
 
     bm_list = msgpack.loads(fp_bm.read())
+
+    # Find the offset of the last date for which we have trading data in our
+    # list of valid trading days
+    last_bm_date = tuple_to_date(bm_list[-1][0])
+    last_bm_date_offset = trading_days.get_loc(
+        last_bm_date.strftime('%Y/%m/%d'))
+
+    # If more than 1 trading days has elapsed since the last day where
+    # we have data,then we need to update
+    if len(trading_days) - last_bm_date_offset > 1:
+        update_benchmarks(bm_symbol, last_bm_date)
+        fp_bm = get_datafile(get_benchmark_filename(bm_symbol), "rb")
+        bm_list = msgpack.loads(fp_bm.read())
+
     bm_returns = []
     for packed_date, returns in bm_list:
         event_dt = tuple_to_date(packed_date)
@@ -126,6 +193,20 @@ Fetching data from data.treasury.gov
         fp_tr = get_datafile('treasury_curves.msgpack', "rb")
 
     tr_list = msgpack.loads(fp_tr.read())
+
+    # Find the offset of the last date for which we have trading data in our
+    # list of valid trading days
+    last_tr_date = tuple_to_date(tr_list[-1][0])
+    last_tr_date_offset = trading_days.get_loc(
+        last_tr_date.strftime('%Y/%m/%d'))
+
+    # If more than 1 trading days has elapsed since the last day where
+    # we have data,then we need to update
+    if len(trading_days) - last_tr_date_offset > 1:
+        update_benchmarks(last_tr_date)
+        fp_tr = get_datafile('treasury_curves.msgpack', "rb")
+        tr_list = msgpack.loads(fp_tr.read())
+
     tr_curves = {}
     for packed_date, curve in tr_list:
         tr_dt = tuple_to_date(packed_date)
