7dfa0d1980c5a32569f206977fdf89ac3a5ab365
Eddie Hebert <ehebert@quantopian.com>
2013-03-27 13:04:24 -0400

    REL: v0.5.8
    - Fixes bug/confusion caused by pandoc conversion in setup.py
    - Slight refactorings, removing missing methods, etc.

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ else:
 
 setup(
     name='zipline',
-    version='0.5.7',
+    version='0.5.8',
     description='A backtester for financial algorithms.',
     author='Quantopian Inc.',
     author_email='opensource@quantopian.com',
