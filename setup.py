82a3388310730d1bae248e4fcd366072966cce4b
Eddie Hebert <ehebert@quantopian.com>
2013-04-02 11:22:31 -0400

    REL: v0.5.9
    Highlights:
    
    - Benchmark updating now permits empty ranges.
      (Fixes runtime crash when running immediately after Easter 2013.)
    - Risk metrics
      - Performance improvents from converting to numpy and pandas.
        <@wesm, wesmckinn@gmail.com>
      - Refactoring of risk metric calculation out of class structure.

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ else:
 
 setup(
     name='zipline',
-    version='0.5.8',
+    version='0.5.9',
     description='A backtester for financial algorithms.',
     author='Quantopian Inc.',
     author_email='opensource@quantopian.com',
