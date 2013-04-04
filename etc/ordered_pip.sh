14f18ccbb40ab13e692dbb557760086cd898bb9e
Jonathan Kamens <jik@quantopian.com>
2013-04-04 11:40:47 -0400

    MAINT: Specify --use-mirrors to pip install

--- etc/ordered_pip.sh
+++ etc/ordered_pip.sh
@@ -4,7 +4,7 @@ a=0
 while read line
 do     
     if [[ -n "$line" && "$line" != \#* ]] ; then
-        pip install $line
+        pip install --use-mirrors $line
     fi
     ((a = a + 1))
 done < $1
