cc200292178077979b8740eb6edf3b1f2e944c23
Jeremiah Lowin <jlowin@lowindata.com>
2013-04-27 09:08:26 -0400

    add AD - CDL3BLACKCROWS

--- zipline/transforms/ta.py
+++ zipline/transforms/ta.py
@@ -32,3 +32,132 @@ def TALibTransform(talib_fn, *args, **kwargs):
     return TALIB_wrapper
 
 
+#========= TA-LIB WRAPPERS
+
+
+def AD(refresh_period=1):
+    """Chaikin A/D Line"""
+    transform = BatchTransform(
+        func=TALibTransform(talib.abstract.AD),
+        refresh_period=refresh_period,
+        window_length=1)
+    return transform
+
+
+def ADOSC(fastperiod=3, slowperiod=10, refresh_period=1):
+    """Chaikin A/D Oscillator"""
+    transform = BatchTransform(
+        func=TALibTransform(talib.abstract.ADOSC, fastperiod, slowperiod),
+        refresh_period=refresh_period,
+        window_length=max(fastperiod, slowperiod))
+    return transform
+
+
+def ADX(timeperiod=14, refresh_period=1):
+    """Average Directional Movement Index"""
+    transform = BatchTransform(
+        func=TALibTransform(talib.abstract.ADX, timeperiod),
+        refresh_period=refresh_period,
+        window_length=timeperiod)
+    return transform
+
+
+def ADXR(timeperiod=14, refresh_period=1):
+    """Average Directional Movement Index Rating"""
+    transform = BatchTransform(
+        func=TALibTransform(talib.abstract.ADXR, timeperiod),
+        refresh_period=refresh_period,
+        window_length=timeperiod)
+    return transform
+
+
+def APO(fastperiod=12, slowperiod=26, matype=0, refresh_period=1):
+    """Absolute Price Oscillator"""
+    transform = BatchTransform(
+        func=TALibTransform(talib.abstract.APO, fastperiod, slowperiod),
+        refresh_period=refresh_period,
+        window_length=max(fastperiod, slowperiod))
+    return transform
+
+
+def AROON(timeperiod=14, refresh_period=1):
+    """Aroon"""
+    transform = BatchTransform(
+        func=TALibTransform(talib.abstract.AROON, timeperiod),
+        refresh_period=refresh_period,
+        window_length=timeperiod)
+    return transform
+
+
+def AROONOSC(timeperiod=14, refresh_period=1):
+    """Aroon Oscillator"""
+    transform = BatchTransform(
+        func=TALibTransform(talib.abstract.AROONOSC, timeperiod),
+        refresh_period=refresh_period,
+        window_length=timeperiod)
+    return transform
+
+
+def ATR(timeperiod=14, refresh_period=1):
+    """Average True Range"""
+    transform = BatchTransform(
+        func=TALibTransform(talib.abstract.ATR, timeperiod),
+        refresh_period=refresh_period,
+        window_length=timeperiod)
+    return transform
+
+
+def AVGPRICE(refresh_period=1):
+    """Average True Range"""
+    transform = BatchTransform(
+        func=TALibTransform(talib.abstract.AVGPRICE),
+        refresh_period=refresh_period,
+        window_length=1)
+    return transform
+
+
+def BBANDS(timeperiod=5, nbdevup=2, nbdevdn=2, matype=0, refresh_period=1):
+    """Bollinger Bands"""
+    transform = BatchTransform(
+        func=TALibTransform(
+            talib.abstract.BBANDS, timeperiod, nbdevup, nbdevdn, matype),
+        refresh_period=refresh_period,
+        window_length=timeperiod)
+    return transform
+
+
+def BOP(refresh_period=1):
+    """Balance of Power"""
+    transform = BatchTransform(
+        func=TALibTransform(talib.abstract.BOP),
+        refresh_period=refresh_period,
+        window_length=1)
+    return transform
+
+
+def CCI(timeperiod=14, refresh_period=1):
+    """Commodity Channel Index"""
+    transform = BatchTransform(
+        func=TALibTransform(talib.abstract.CCI, timeperiod),
+        refresh_period=refresh_period,
+        window_length=timeperiod)
+    return transform
+
+
+def CDL2CROWS(refresh_period=1):
+    """Two Crows"""
+    transform = BatchTransform(
+        func=TALibTransform(talib.abstract.CDL2CROWS),
+        refresh_period=refresh_period,
+        window_length=1)
+    return transform
+
+
+def CDL3BLACKCROWS(refresh_period=1):
+    """Two Crows"""
+    transform = BatchTransform(
+        func=TALibTransform(talib.abstract.CDL2CROWS),
+        refresh_period=refresh_period,
+        window_length=1)
+    return transform
+
