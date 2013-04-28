1ac54cb4f45565fe423a4cb42f7575fd08989f94
Jeremiah Lowin <jlowin@lowindata.com>
2013-04-28 19:26:05 -0400

    BUG: was accessing lookback without setting parameters; had to make copy of function to solve

--- zipline/transforms/ta.py
+++ zipline/transforms/ta.py
@@ -1,4 +1,5 @@
 import talib
+import copy
 from zipline.transforms import BatchTransform
 
 
@@ -7,12 +8,27 @@ def make_transform(talib_fn):
     A factory for BatchTransforms based on TALIB abstract functions.
     """
     class TALibTransform(BatchTransform):
-        def __init__(self, sid, *args, **kwargs):
+        def __init__(self, sid, **kwargs):
+            # check for BatchTransform refresh_period
             refresh_period = kwargs.pop('refresh_period', 0)
-            self.call_args = args
-            self.call_kwargs = kwargs
 
-            self.talib_fn = talib_fn
+            # Make deepcopy of talib abstract function.
+            # This is necessary because talib abstract functions remember
+            # state, including parameters, and we need to set the parameters
+            # in order to compute the lookback period that will determine the
+            # BatchTransform window_length. TALIB has no way to restore default
+            # parameters, so the deepcopy lets us change this function's
+            # parameters without affecting other TALibTransforms of the same
+            # function.
+            self.talib_fn = copy.deepcopy(talib_fn)
+
+            # set the parameters
+            for param in self.talib_fn.get_parameters().keys():
+                if param in kwargs:
+                    self.talib_fn.set_parameters({param : kwargs[param]})
+
+            # get the lookback
+            self.lookback = self.talib_fn.lookback
 
             def zipline_wrapper(data):
                 # convert zipline dataframe to talib data_dict
@@ -28,9 +44,7 @@ def make_transform(talib_fn):
                     data_dict['close'] = data['price'].values[:, 0]
 
                 # call talib
-                result = self.talib_fn(data_dict,
-                                       self.call_args,
-                                       **self.call_kwargs)
+                result = self.talib_fn(data_dict)
 
                 # keep only the most recent result
                 if isinstance(result, (list, tuple)):
@@ -42,7 +56,7 @@ def make_transform(talib_fn):
                 func=zipline_wrapper,
                 sids=sid,
                 refresh_period=refresh_period,
-                window_length=max(1, self.talib_fn.lookback))
+                window_length=max(1, self.lookback))
 
         def __repr__(self):
             return 'Zipline BatchTransform: {0}'.format(
