24019de573c4e25d7392d1b9a9945860bb28d3f1
Eddie Hebert <ehebert@quantopian.com>
2013-05-08 20:58:28 -0400

    BUG: Paper over multiple types for algorithm returns.
    The Seriess-style indexing causes a crash on the list during unit tests.
    
    TODO: Investigate whether the list type of algorithm returns can
          be removed.

--- zipline/finance/risk.py
+++ zipline/finance/risk.py
@@ -837,8 +837,13 @@ class RiskReport(object):
             start_date = self.sim_params.period_start
             end_date = self.sim_params.period_end
         else:
-            start_date = self.algorithm_returns.index[0]
-            end_date = self.algorithm_returns.index[-1]
+            # FIXME: Papering over multiple algorithm_return types
+            if isinstance(self.algorithm_returns, list):
+                start_date = self.algorithm_returns[0].date
+                end_date = self.algorithm_returns[-1].date
+            else:
+                start_date = self.algorithm_returns.index[0]
+                end_date = self.algorithm_returns.index[-1]
 
         self.month_periods = self.periods_in_range(1, start_date, end_date)
         self.three_month_periods = self.periods_in_range(3, start_date,
