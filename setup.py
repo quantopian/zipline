4c6b9b945df856fe6885a11e1a4363364fc28685
Eddie Hebert <ehebert@quantopian.com>
2014-01-08 10:00:31 -0500

    REL: Release v0.6.0
    Highlights
    
    - Reworked risk metrics, including verification against Excel spreadsheet
    - Additional order methods
    - Conversion of many data structures to use pandas
    - Change to behavior of stop and limit orders (@pcawthron)
    - Use pandas timezone handling throughout instead of Delorean
    - New commission model (@stanh)
    - Adds beginning of support for Toronto stock exchange. (@dstephens)
    - Python 3 compatibility.
      Unit tests now pass when run with Python 3.3, and Python 3 should
      now be considered officially supported.
      If anything does not work under Python 3, please file as a bug.

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ else:
 
 setup(
     name='zipline',
-    version='0.5.11.dev',
+    version='0.6.0',
     description='A backtester for financial algorithms.',
     author='Quantopian Inc.',
     author_email='opensource@quantopian.com',
