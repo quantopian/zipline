ad241a3df137d08e32d42ba9dbde3a710647aec8
twiecki <thomas.wiecki@gmail.com>
2014-02-11 14:21:44 -0500

    BLD: Added conda build files.

---
+++ conda/ta-lib/build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+wget -O ta-lib-0.4.0-src.tar.gz http://sourceforge.net/projects/ta-lib/files/ta-lib/0.4.0/ta-lib-0.4.0-src.tar.gz/download
+tar xvzf ta-lib-0.4.0-src.tar.gz
+pushd ta-lib
+./configure --prefix=$PREFIX
+make
+make install
+popd
+
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$PREFIX/lib
+python setup.py build_ext
+
+python setup.py install
