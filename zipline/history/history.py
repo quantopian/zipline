e261438d01e0844b6d9c8c019c7e11f44cb37ac1
twiecki <thomas.wiecki@gmail.com>
2014-03-17 11:34:53 +0900

    ENH: Adapt history() to work on zipline.

--- zipline/history/history.py
+++ zipline/history/history.py
@@ -1,3 +1,18 @@
+#
+# Copyright 2014 Quantopian, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 from __future__ import division
 
 import numpy as np
@@ -51,7 +66,7 @@ class HistorySpec(object):
     def __init__(self, bar_count, frequency, field, ffill):
         # Number of bars to look back.
         self.bar_count = bar_count
-        if isinstance(frequency, basestring):
+        if isinstance(frequency, str):
             frequency = Frequency(frequency)
         # The frequency at which the data is sampled.
         self.frequency = frequency
