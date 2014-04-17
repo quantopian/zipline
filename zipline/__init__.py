85053f7536ed1960d2cfd7edcf7430fc788206e8
twiecki <thomas.wiecki@gmail.com>
2014-04-17 15:49:31 -0400

    BLD: Wrong version in __init__.py

--- zipline/__init__.py
+++ zipline/__init__.py
@@ -20,7 +20,7 @@ Zipline
 # This is *not* a place to dump arbitrary classes/modules for convenience,
 # it is a place to expose the public interfaces.
 
-__version__ = "0.5.11.dev"
+__version__ = "0.6.0"
 
 from . import data
 from . import finance
