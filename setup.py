b38bf07a95619ddf46e28c0e40bc3bccc797ad70
twiecki <thomas.wiecki@gmail.com>
2014-04-17 15:49:33 -0400

    Bump version: 0.6.0 → 0.6.1

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ else:
 
 setup(
     name='zipline',
-    version='0.6.0',
+    version='0.6.1',
     description='A backtester for financial algorithms.',
     author='Quantopian Inc.',
     author_email='opensource@quantopian.com',
