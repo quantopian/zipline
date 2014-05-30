b6e5345893440dcff1a40f9f4d6dbe9b6632e160
Scott Sanderson <scoutoss@gmail.com>
2014-05-30 18:24:24 -0400

    ENH: Enhancements to `TradingEnvironment`.
    Adds a suite of new functions for querying data from the trading calendar.
    
    These include:
          `previous_trading_day`
          `minutes_for_days_in_range` (minutely version of `days_in_range`)
          `previous_open_and_close` (inverse of `next_open_and_close`)
          `next_market_minute`
          `previous_market_minute`
          `open_close_window` (get a range of opens/closes with slicing semantics)
          `market_minute_window` (get a range of minutes with slicing semantics)
    
    Also refactors `test_finance` to move `TradingEnvironment` tests into their own
    TestCase.

--- zipline/finance/trading.py
+++ zipline/finance/trading.py
@@ -18,6 +18,7 @@ import logbook
 import datetime
 
 import pandas as pd
+import numpy as np
 
 from zipline.data.loader import load_market_data
 from zipline.utils import tradingcalendar
@@ -174,10 +175,38 @@ class TradingEnvironment(object):
 
         return None
 
+    def previous_trading_day(self, test_date):
+        dt = self.normalize_date(test_date)
+        delta = datetime.timedelta(days=-1)
+
+        while self.first_trading_day < test_date:
+            dt += delta
+            if dt in self.trading_days:
+                return dt
+
+        return None
+
     def days_in_range(self, start, end):
         mask = ((self.trading_days >= start) &
                 (self.trading_days <= end))
         return self.trading_days[mask]
+
+    def minutes_for_days_in_range(self, start, end):
+        """
+        Get all market minutes for the days between start and end, inclusive.
+        """
+        start_date = self.normalize_date(start)
+        end_date = self.normalize_date(end)
+
+        all_minutes = []
+        for day in self.days_in_range(start_date, end_date):
+            day_minutes = self.market_minutes_for_day(day)
+            all_minutes.append(day_minutes)
+
+        # Concatenate all minutes and truncate minutes before start/after end.
+        return pd.DatetimeIndex(
+            np.concatenate(all_minutes), copy=False, tz='UTC',
+        )
 
     def next_open_and_close(self, start_date):
         """
@@ -193,14 +222,103 @@ Last successful date: %s" % self.last_trading_day)
 
         return self.get_open_and_close(next_open)
 
+    def previous_open_and_close(self, start_date):
+        """
+        Given the start_date, returns the previous open and close of the
+        market.
+        """
+        previous = self.previous_trading_day(start_date)
+
+        if previous is None:
+            raise NoFurtherDataError(
+                "Attempt to backtest beyond available history. "
+                "First successful date: %s" % self.first_trading_day)
+        return self.get_open_and_close(previous)
+
+    def next_market_minute(self, start):
+        """
+        Get the next market minute after @start. This is either the immediate
+        next minute, or the open of the next market day after start.
+        """
+        next_minute = start + datetime.timedelta(minutes=1)
+        if self.is_market_hours(next_minute):
+            return next_minute
+        return self.next_open_and_close(start)[0]
+
+    def previous_market_minute(self, start):
+        """
+        Get the next market minute before @start. This is either the immediate
+        previous minute, or the close of the market day before start.
+        """
+        prev_minute = start - datetime.timedelta(minutes=1)
+        if self.is_market_hours(prev_minute):
+            return prev_minute
+        return self.previous_open_and_close(start)[1]
+
     def get_open_and_close(self, day):
         todays_minutes = self.open_and_closes.ix[day.date()]
 
         return todays_minutes['market_open'], todays_minutes['market_close']
 
-    def market_minutes_for_day(self, midnight):
-        market_open, market_close = self.get_open_and_close(midnight)
+    def market_minutes_for_day(self, stamp):
+        market_open, market_close = self.get_open_and_close(stamp)
         return pd.date_range(market_open, market_close, freq='T')
+
+    def open_close_window(self, start, count, offset=0, step=1):
+        """
+        Return a DataFrame containing `count` market opens and closes,
+        beginning with `start` + `offset` days and continuing `step` minutes at
+        a time.
+        """
+        # TODO: Correctly handle end of data.
+        start_idx = self.get_index(start) + offset
+        stop_idx = start_idx + (count * step)
+
+        index = np.arange(start_idx, stop_idx, step)
+
+        return self.open_and_closes.iloc[index]
+
+    def market_minute_window(self, start, count, step=1):
+        """
+        Return a DatetimeIndex containing `count` market minutes, starting with
+        `start` and continuing `step` minutes at a time.
+        """
+        if not self.is_market_hours(start):
+            raise ValueError("market_minute_window starting at "
+                             "non-market time {minute}".format(minute=start))
+
+        all_minutes = []
+
+        current_day_minutes = self.market_minutes_for_day(start)
+        first_minute_idx = current_day_minutes.searchsorted(start)
+        minutes_in_range = current_day_minutes[first_minute_idx::step]
+
+        # Build up list of lists of days' market minutes until we have count
+        # minutes stored altogether.
+        while True:
+
+            if len(minutes_in_range) >= count:
+                # Truncate off extra minutes
+                minutes_in_range = minutes_in_range[:count]
+
+            all_minutes.append(minutes_in_range)
+            count -= len(minutes_in_range)
+            if count <= 0:
+                break
+
+            if step > 0:
+                start, _ = self.next_open_and_close(start)
+                current_day_minutes = self.market_minutes_for_day(start)
+            else:
+                _, start = self.previous_open_and_close(start)
+                current_day_minutes = self.market_minutes_for_day(start)
+
+            minutes_in_range = current_day_minutes[::step]
+
+        # Concatenate all the accumulated minutes.
+        return pd.DatetimeIndex(
+            np.concatenate(all_minutes), copy=False, tz='UTC',
+        )
 
     def trading_day_distance(self, first_date, second_date):
         first_date = self.normalize_date(first_date)
