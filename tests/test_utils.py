39a4927925d35ec85df9aafcf87c1333eb7cb33a
David Edwards <dedwards@rohan.sdsu.edu>
2014-07-10 19:05:54 -0700

    TST: Added unittest for EntryRules to test_utils
    Added a unittest for the entry rules. These should be separate from the
    EventManager tests because they can be used independently as well.
    
    I used a lot of numpy boolean array slicing and apparently flake8 hates
    me for it.
    
    e.g. array[array == False] should be 'if cond is False' or 'if not
    cond.
    I don’t think there is a fix other than rewriting the tests. Let me
    know if this can be ignored or not.

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -13,9 +13,19 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import datetime
 from unittest import TestCase
+from zipline.utils import tradingcalendar
 from zipline.utils.factory import (load_from_yahoo,
                                    load_bars_from_yahoo)
+from zipline.utils.event_management import(
+    AfterOpen,
+    BeforeClose,
+    AtTime,
+    BetweenTimes,
+    at_market_open,
+    at_market_close
+)
 import pandas as pd
 import pytz
 import numpy as np
@@ -56,3 +66,82 @@ class TestFactory(TestCase):
             AssertionError, load_bars_from_yahoo, stocks=stocks,
             start=end, end=start
         )
+
+
+class TestEntryRules(TestCase):
+
+    def setUp(self):
+        open_close = tradingcalendar.open_and_closes.iloc[0]
+        self.open_time = open_close['market_open']
+        self.close_time = open_close['market_close']
+        self.interval = datetime.timedelta(minutes=1)
+        market_mins = []
+        dt = self.open_time
+        while dt <= self.close_time:
+            market_mins.append(dt)
+            dt += self.interval
+        self.market_mins = market_mins
+
+    def test_AfterOpen(self):
+        """
+        Only the first 65 minutes in the trading day
+        should evaluate to False.
+        """
+        after_open = AfterOpen(minutes=5, hours=1)
+        results = pd.Series(
+            {dt: after_open(dt) for dt in self.market_mins}
+        )
+        invalid_times = results[results == False]
+        assert len(invalid_times) == 65
+        assert np.all(invalid_times.index == self.market_mins[0:65])
+
+    def test_BeforeClose(self):
+        """
+        Only the last 65 minutes in the trading day
+        should evaluate to True.
+        """
+        before_close = BeforeClose(minutes=5, hours=1)
+        results = pd.Series(
+            {dt: before_close(dt) for dt in self.market_mins}
+        )
+        valid_times = results[results == True]
+        assert len(valid_times) == 65
+        assert np.all(valid_times.index == self.market_mins[-65::])
+
+    def test_AtTime(self):
+        # 10:00 EST / 15:00 UTC test date
+        testdt = pd.Timestamp('1990-01-02 15:00:00+0000', tz='UTC')
+        at_time = AtTime(hour=10, minute=0, tz='US/Eastern')
+        results = pd.Series({i: at_time(i) for i in self.market_mins})
+        valid_times = results[results == True]
+        assert len(valid_times) == 1
+        assert testdt in valid_times
+
+    def test_BetweenTimes(self):
+        # 10:00 EST / 15:00 UTC to 5 min later test times
+        bw_times = BetweenTimes((10, 0), (10, 5), tz='US/Eastern')
+        testdts = pd.Index([
+            pd.Timestamp('1990-01-02 15:00:00+0000', tz='UTC'),
+            pd.Timestamp('1990-01-02 15:01:00+0000', tz='UTC'),
+            pd.Timestamp('1990-01-02 15:02:00+0000', tz='UTC'),
+            pd.Timestamp('1990-01-02 15:03:00+0000', tz='UTC'),
+            pd.Timestamp('1990-01-02 15:04:00+0000', tz='UTC')
+        ])
+        results = pd.Series({i: bw_times(i) for i in self.market_mins})
+        valid_times = results[results == True]
+        assert len(valid_times) == 5
+        assert np.all(testdts == valid_times.index)
+
+    def test_at_market_open(self):
+        results = pd.Series({dt: at_market_open(dt)
+                             for dt in self.market_mins})
+        valid_times = results[results == True]
+        assert len(valid_times) == 1
+        assert self.open_time in valid_times
+
+    def test_at_market_close(self):
+        results = pd.Series({dt: at_market_close(dt)
+                             for dt in self.market_mins})
+        valid_times = results[results == True]
+        assert len(valid_times) == 1
+        assert self.close_time in valid_times
