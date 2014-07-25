a7de02176b3d8f78cc38253bd08432e9a04332cd
Thomas Wiecki <thomas.wiecki@gmail.com>
2014-07-25 19:34:49 +0200

    Bump version: 0.7.0rc1 → 0.7.0

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ else:
 
 setup(
     name='zipline',
-    version='0.7.0rc1',
+    version='0.7.0',
     description='A backtester for financial algorithms.',
     author='Quantopian Inc.',
     author_email='opensource@quantopian.com',
