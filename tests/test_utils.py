702c00bbf765dfb1f6aefb406b9f6cf700460980
David Edwards <mail.dedwards@gmail.com>
2014-09-01 11:53:19 -0700

    TST: Added tests for market timing rules
    Tests were added for the following market timing rules.
    AfterOpen
    BeforeClose
    AtTime
    BetweenTimes
    market_open
    market_close

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -20,6 +20,18 @@ import pandas as pd
 import pytz
 import numpy as np
 
+import random
+from zipline.finance.trading import TradingEnvironment
+
+from zipline.utils.events import (
+    AfterOpen,
+    BeforeClose,
+    AtTime,
+    BetweenTimes,
+    market_open,
+    market_close,
+)
+
 
 class TestFactory(TestCase):
     def test_load_from_yahoo(self):
@@ -56,3 +68,71 @@ class TestFactory(TestCase):
             AssertionError, load_bars_from_yahoo, stocks=stocks,
             start=end, end=start
         )
+
+
+class TestMarketTimingRules(TestCase):
+
+    def setUp(self):
+        env = TradingEnvironment.instance()
+        # Select a random sample of 5 trading days
+        index = random.sample(range(len(env.trading_days)), 5)
+        test_dts = [env.trading_days[i] for i in index]
+        self.open_close_times = [env.get_open_and_close(dt)
+                                 for dt in test_dts]
+        self.market_minutes = [env.market_minutes_for_day(dt)
+                               for dt in test_dts]
+
+    def test_AfterOpen(self):
+        """
+        Only the first 65 minutes in the trading day
+        should evaluate to False.
+        """
+        after_open = AfterOpen(minutes=5, hours=1)
+        for trading_day in self.market_minutes:
+            for dt in trading_day[0:65]:
+                assert after_open(dt) is False
+            for dt in trading_day[65::]:
+                assert after_open(dt) is True
+
+    def test_BeforeClose(self):
+        """
+        Only the last 65 minutes in the trading day
+        should evaluate to True.
+        """
+        before_close = BeforeClose(minutes=5, hours=1)
+        for i, trading_day in enumerate(self.market_minutes):
+            for dt in trading_day[0:-65]:
+                assert before_close(dt) is False
+            for dt in trading_day[-65::]:
+                assert before_close(dt) is True
+
+    def test_AtTime(self):
+        # Only the 9th minute on any trading day should be True.
+        at_time = AtTime(hour=9, minute=40, tz='US/Eastern')
+        for minutes in self.market_minutes:
+            for minute in minutes:
+                if at_time(minute):
+                    assert minutes.searchsorted(minute) == 9
+
+    def test_BetweenTimes(self):
+        # criteria: t1 <= dt < t2
+        bw_times = BetweenTimes((9, 40), (9, 50), tz='US/Eastern')
+        indexes = range(9, 19)
+        for minutes in self.market_minutes:
+            for minute in minutes:
+                if bw_times(minute):
+                    assert minutes.searchsorted(minute) in indexes
+
+    def test_market_open(self):
+        for i, minutes in enumerate(self.market_minutes):
+            open_time = self.open_close_times[i][0]
+            for minute in minutes:
+                if market_open(minute):
+                    assert minute == open_time
+
+    def test_market_close(self):
+        for i, minutes in enumerate(self.market_minutes):
+            close_time = self.open_close_times[i][1]
+            for minute in minutes:
+                if market_close(minute):
+                    assert minute == close_time
