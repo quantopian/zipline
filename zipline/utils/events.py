09d2c9cc421da9b13184658e049f870d512aae7e
David Edwards <mail.dedwards@gmail.com>
2014-09-02 20:50:04 -0700

    MAINT: cleaned up EventOffset class
    Changed the default start_dt argument in EventOffset
    to None.
    
    EventOffset.handle_data now returns True
    if the event is triggered, this was to make testing
    more convenient.

--- zipline/utils/events.py
+++ zipline/utils/events.py
@@ -19,8 +19,7 @@ class EventContainer(object):
 
     events = []
 
-    def add_event(self, rule=None, func=None,
-                  freq=None, start_dt=tradingcalendar.start):
+    def add_event(self, rule=None, func=None, freq=None, start_dt=None):
 
         event = EventOffset(
             rule=rule,
@@ -30,6 +29,10 @@ class EventContainer(object):
         self.events.append(event)
 
     def handle_data(self, context, data, dt):
+        """
+        Calls registered events with the current
+        algo datetime.
+        """
         for event in self.events:
             event.handle_data(context, data, dt)
 
@@ -40,8 +43,7 @@ class EventOffset(object):
     other than the backtest frequency.
     """
 
-    def __init__(self, rule=None, func=None,
-                 freq=None, start_dt=tradingcalendar.start):
+    def __init__(self, rule=None, func=None, freq=None, start_dt=None):
         """
         Params:
             rule: callable <defalult=None>
@@ -57,7 +59,7 @@ class EventOffset(object):
             freq: An object that can be converted to a pandas offset
                   via pandas.tseries.frequencies.to_offset
 
-            start_dt: tz-aware datetime obj
+            start_dt: tz-aware datetime obj <default=None>
                   The first datetime the event can occur.
         """
         if rule is None:
@@ -68,9 +70,12 @@ class EventOffset(object):
             self._funcs = list(func)
         else:
             self._funcs = [func]
-        self.offset = to_offset(freq)
-        self.start_dt = start_dt
+        if start_dt is None:
+            self.start_dt = tradingcalendar.start
+        else:
+            self.start_dt = start_dt.astimezone(pytz.utc)
         self.event_dt = self.start_dt
+        self.offset = to_offset(freq)
 
     def handle_data(self, context, data, dt):
         if dt < self.event_dt:
@@ -79,6 +84,7 @@ class EventOffset(object):
             for func in self._funcs:
                 func(context, data)
             self.event_dt = self.offset.apply(dt)
+            return True
 
 
 #
