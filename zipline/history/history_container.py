c921ebc1c4e5e784453e7402728e06899881e4a4
Dale Jung <dale@dalejung.com>
2015-02-06 11:19:18 -0500

    Backported pandas 0.15.2 interpolation to make compatible with 0.12.0
    fix method call changes

--- zipline/history/history_container.py
+++ zipline/history/history_container.py
@@ -42,8 +42,46 @@ def _interpolate(values, method, axis=None):
         axis = 1
     else:
         raise Exception("Cannot interpolate array with more than 2 dims")
+
     values = values.copy()
-    values = com.interpolate_2d(values, method, axis=axis)
+    values = interpolate_2d(values, method, axis=axis)
+    return values
+
+
+def interpolate_2d(values, method='pad', axis=0, limit=None, fill_value=None):
+    """
+    Copied from the 0.15.2. This did not exist in 0.12.0.
+
+    Differences:
+        - Don't depend on pad_2d and backfill_2d to return values
+        - Removed dtype kwarg. 0.12.0 did not have this option.
+    """
+    transf = (lambda x: x) if axis == 0 else (lambda x: x.T)
+
+    # reshape a 1 dim if needed
+    ndim = values.ndim
+    if values.ndim == 1:
+        if axis != 0:  # pragma: no cover
+            raise AssertionError("cannot interpolate on a ndim == 1 with "
+                                 "axis != 0")
+        values = values.reshape(tuple((1,) + values.shape))
+
+    if fill_value is None:
+        mask = None
+    else:  # todo create faster fill func without masking
+        mask = com.mask_missing(transf(values), fill_value)
+
+    # Note: pad_2d and backfill_2d work inplace in 0.12.0 and 0.15.2
+    # in 0.15.2 they also return a reference to values
+    if method == 'pad':
+        com.pad_2d(transf(values), limit=limit, mask=mask)
+    else:
+        com.backfill_2d(transf(values), limit=limit, mask=mask)
+
+    # reshape back
+    if ndim == 1:
+        values = values[0]
+
     return values
 
 
