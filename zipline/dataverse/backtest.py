6caecee993a1b414fff7bdb5675fdd696c4c9c2f
Dale Jung <dale@dalejung.com>
2015-03-13 00:14:29 -0400

    first pass on byassing history

--- zipline/dataverse/backtest.py
+++ zipline/dataverse/backtest.py
@@ -1,16 +1,76 @@
 import zipline.protocol as zp
 from zipline.dataverse.dataverse import BaseDataverse
-from zipline.history.history_container import HistoryContainer
+from zipline.history.history_container import (
+    HistoryContainer,
+    HistoryContainerDelta
+)
+
+
+class HistoryFrame(object):
+    def __init__(self):
+        pass
 
 
 class BacktestHistoryContainer(HistoryContainer):
+    def __init__(self, *args, **kwargs):
+        self.dataverse = kwargs.pop('dataverse')
+        # need to make this cache easier to update
+        # maybe move to chunking?
+        source = self.dataverse.source
+        values = source.values
+        self.values = values
+        self.source = source
+        self.get_loc = source.major_axis.get_loc
+        self.cache = {}
+
     def get_history(self, history_spec, algo_dt):
-        return super(BacktestHistoryContainer, self).get_history(history_spec,
-                                                                 algo_dt)
+        # do this by regions
+        loc = self.get_loc(algo_dt)
+        start = max(loc - history_spec.bar_count, 0)
+        sl = slice(start, loc)
+
+        try:
+            data = self.cache[history_spec]
+            values = self.values
+            vals = values[:, sl]
+            block = data._data.blocks[0]
+            if vals.flags.f_contiguous != block.values.flags.f_contiguous:
+                vals = vals.T
+            data._data.blocks[0].values = vals
+            data._item_cache.clear()
+        except KeyError:
+            data = self.source.iloc[:, sl]
+            # data = HistoryFrame(data)
+            self.cache[history_spec] = data
+        return data
+
+    def ensure_spec(self, spec, dt, bar_data):
+        """
+        Ensure that this container has enough space to hold the data for the
+        given spec. This returns a HistoryContainerDelta to represent the
+        changes in shape that the container made to support the new
+        HistorySpec.
+        """
+        updated = {}
+        return HistoryContainerDelta(**updated)
+
+    def update(self, data, algo_dt):
+        pass
 
 
 class BacktestDataverse(BaseDataverse):
     history_container_class = BacktestHistoryContainer
+
+    def get_history_container(self, *args, **kwargs):
+        kwargs['dataverse'] = self
+        return self.history_container_class(*args, **kwargs)
+
+    def get_source(self, source, overwrite_sim_params=True):
+        self.source = source
+        return super(BacktestDataverse, self).get_source(source)
+
+    def update_universe(self, event):
+        pass
 
 
 class SIDData(object):
