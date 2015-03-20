27812e3fe00aac1dbdfd17bcecb18720dcb0cc9c
Warren O'Neill <warren-oneill@users.noreply.github.com>
2015-03-20 13:45:21 +0100

    Renaming tc_td and tc_tday

--- zipline/finance/trading.py
+++ zipline/finance/trading.py
@@ -92,12 +92,13 @@ class TradingEnvironment(object):
         max_date=None,
         env_trading_calendar=tradingcalendar
     ):
-        # `tc_td` is short for "trading calendar trading days"
-        tc_td = env_trading_calendar.trading_days
+        # `tc_tdays` is short for "trading calendar trading days"
+        # `tc_tday` is short for "trading calendar trading day" 
+        tc_tdays = env_trading_calendar.trading_days
         tc_tday = env_trading_calendar.trading_day
 
         if max_date:
-            self.trading_days = tc_td[tc_td <= max_date].copy()
+            self.trading_days = tc_tdays[tc_td <= max_date].copy()
             self.trading_day = tc_tday[tc_tday <= max_date].copy()
         else:
             self.trading_days = tc_td.copy()
