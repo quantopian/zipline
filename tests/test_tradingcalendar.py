0d2fb168d13d4fa2ce2204d3226755b176ddd299
warren-oneill <warren.oneill@grundgruen.com>
2015-03-26 10:44:31 +0100

    make calendar test compare generated calendar with the benchmark calendar
    renaming cal_days to bench_days

--- tests/test_tradingcalendar.py
+++ tests/test_tradingcalendar.py
@@ -26,7 +26,6 @@ from nose.tools import nottest
 
 class TestTradingCalendar(TestCase):
 
-    @nottest
     def test_calendar_vs_environment(self):
         """
         test_calendar_vs_environment checks whether the
@@ -36,11 +35,11 @@ class TestTradingCalendar(TestCase):
         """
 
         env = TradingEnvironment()
-        env_start_index = \
-            env.trading_days.searchsorted(tradingcalendar.start)
-        env_days = env.trading_days[env_start_index:]
-        cal_days = tradingcalendar.trading_days
-        self.check_days(env_days, cal_days)
+        bench_days = env.benchmark_returns[tradingcalendar.start:].index
+        bounds = env.trading_days.slice_locs(start=tradingcalendar.start,
+                                             end=bench_days[-1])
+        env_days = env.trading_days[bounds[0]:bounds[1]]
+        self.check_days(env_days, bench_days)
 
     @nottest
     def test_lse_calendar_vs_environment(self):
