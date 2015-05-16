1e76be611b5c2f980375f3d850e722b7274c3a39
Eddie Hebert <ehebert@quantopian.com>
2015-05-15 23:31:28 -0400

    MAINT: Remove unused method on performance period.
    set_positions is no longer referenced.

--- zipline/finance/performance/period.py
+++ zipline/finance/performance/period.py
@@ -237,10 +237,6 @@ class PerformancePeriod(object):
         raise ProxyError()
 
     @position_proxy
-    def set_positions(self):
-        raise ProxyError()
-
-    @position_proxy
     def _longs_count(self):
         raise ProxyError()
 
