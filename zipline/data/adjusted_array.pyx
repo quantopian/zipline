fa3b65bf88fc57f9df68cf1c4fc73d7892720c40
Scott Sanderson <scoutoss@gmail.com>
2015-05-26 11:24:33 -0400

    DEV: Add support for adjustment masks.

--- zipline/data/adjusted_array.pyx
+++ zipline/data/adjusted_array.pyx
@@ -4,10 +4,14 @@ Class capable of yielding adjusted chunks of an ndarray.
 from numpy import (
     asarray,
     float64,
+    full,
+    uint8,
 )
+from numpy.ma import masked_array
 from numpy cimport (
     float64_t,
     ndarray,
+    uint8_t,
 )
 
 from zipline.errors import (
@@ -16,13 +20,17 @@ from zipline.errors import (
 )
 
 
-cpdef adjusted_array(ndarray data, dict adjustments):
+cpdef adjusted_array(ndarray data, uint8_t[:, :] mask, dict adjustments):
     """
     Factory function for producing adjusted arrays on inputs of different
     dtypes.
+
+    If mask is None, the array is assumed to contain all valid data points.
+    Otherwise mask should be an array of uint8 of the same shape
+    as data, containing 0s for valid values and 1s for invalid values.
     """
     if data.dtype == float64:
-        return Float64AdjustedArray(data, adjustments)
+        return Float64AdjustedArray(data, mask, adjustments)
     else:
         raise TypeError(
             "Can't generate window iterator for array of dtype %s" % data.dtype
@@ -43,19 +51,27 @@ cdef class Float64AdjustedArray:
     Adjusted array of float64.
     """
     cdef:
-        readonly float64_t[:, ::1] data
-        readonly dict adjustments
+        float64_t[:, :] data
+        uint8_t[:, :] mask
+        dict adjustments
 
     def __cinit__(self,
-                  float64_t[:, ::1] data not None,
+                  float64_t[:, :] data not None,
+                  uint8_t[:, :] mask,  # None is equivalent to all 1s.
                   dict adjustments):
 
         self.data = data
         self.adjustments = adjustments
+        if mask is None:
+            self.mask = full((data.shape[0], data.shape[1]), 1, dtype=uint8)
+        else:
+            assert mask.shape == data.shape
+            self.mask = mask
 
     cpdef traverse(self, int lookback):
         return _Float64AdjustedArrayWindow(
             self.data.copy(),
+            self.mask.copy(),
             lookback,
             self.adjustments,
         )
@@ -73,20 +89,23 @@ cdef class _Float64AdjustedArrayWindow:
     data.
     """
 
-    cdef float64_t[:, ::1] data
+    cdef float64_t[:, :] data
+    cdef uint8_t[:, :] mask
     cdef readonly int lookback
     cdef Py_ssize_t anchor, max_anchor, next_adj
     cdef dict adjustments
     cdef list adjustment_indices
 
     def __cinit__(self,
-                  float64_t[:, ::1] data,
+                  float64_t[:, :] data,
+                  uint8_t[:, :] mask,
                   int lookback,
                   dict adjustments):
 
         _check_lookback(data, lookback)
 
         self.data = data
+        self.mask = mask
         self.lookback = lookback
 
         # anchor is the index of the row **after** the row from which we're
@@ -95,12 +114,11 @@ cdef class _Float64AdjustedArrayWindow:
         self.max_anchor = data.shape[0]
 
         self.adjustments = adjustments
+        self.adjustment_indices = sorted(adjustments, reverse=True)
 
-        if len(self.adjustments) > 0:
-            self.adjustment_indices = sorted(adjustments, reverse=True)
+        if len(self.adjustment_indices) > 0:
             self.next_adj = self.adjustment_indices.pop()
         else:
-            self.adjustment_indices = []
             self.next_adj = self.max_anchor
 
     def __iter__(self):
@@ -110,28 +128,32 @@ cdef class _Float64AdjustedArrayWindow:
         cdef:
             ndarray[float64_t, ndim=2] out
             object adjustment
+            Py_ssize_t start, anchor
 
-        if self.anchor > self.max_anchor:
+        anchor = self.anchor
+        if anchor > self.max_anchor:
             raise StopIteration()
 
         # Apply any adjustments that occured before our current anchor.
         # Equivalently, apply any adjustments known on or before the date for
         # which we're calculating a window.
-        while self.next_adj < self.anchor:
-            print self.next_adj, self.anchor, self.adjustment_indices
+        while self.next_adj < anchor:
+
             for adjustment in self.adjustments[self.next_adj]:
                 adjustment.mutate(self.data)
+                adjustment.mutate_mask(self.mask)
 
             if len(self.adjustment_indices) > 0:
                 self.next_adj = self.adjustment_indices.pop()
             else:
-                # No more adjustments to apply.
                 self.next_adj = self.max_anchor
 
-        out = asarray(self.data[self.anchor - self.lookback: self.anchor])
-        out.setflags(write=0)
+        start = anchor - self.lookback
+        out = asarray(self.data[start: self.anchor])
+        mask_out = asarray(self.mask[start: self.anchor])
+
         self.anchor += 1
-        return out
+        return out, mask_out
 
     def __repr__(self):
         return "%s(lookback=%d, anchor=%d, max_anchor=%d)" % (
