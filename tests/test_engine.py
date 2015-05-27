870e320fa52d44d0cd32a51e66020001c86b5c8b
Scott Sanderson <scoutoss@gmail.com>
2015-05-27 17:29:27 -0400

    TEST: Use USEquityPricing instead of SomeDataSet in tests.

--- tests/test_engine.py
+++ tests/test_engine.py
@@ -4,20 +4,14 @@ Tests for SimpleFFCEngine
 from unittest import TestCase
 
 from numpy import (
-    float32,
     full,
-    uint32,
-    uint8,
 )
 from numpy.testing import assert_array_equal
 from pandas import (
     date_range,
 )
 
-from zipline.data.dataset import (
-    Column,
-    DataSet,
-)
+from zipline.data.equities import USEquityPricing
 from zipline.data.synthetic import (
     ConstantLoader,
 )
@@ -25,24 +19,13 @@ from zipline.modelling.engine import SimpleFFCEngine
 from zipline.modelling.factor import TestFactor
 
 
-class SomeDataSet(DataSet):
-
-    foo = Column(float32)
-    bar = Column(uint32)
-    buzz = Column(uint8)
-
-
-def sum_all(foo, bar):
-    return (foo + bar).sum()
-
-
 class SumDifference(TestFactor):
     window_length = 3
-    inputs = [SomeDataSet.foo, SomeDataSet.bar]
+    inputs = [USEquityPricing.open, USEquityPricing.close]
 
     @staticmethod
-    def compute(foo, bar):
-        return (foo - bar).sum(axis=0)
+    def from_windows(open, close):
+        return (open - close).sum(axis=0)
 
 
 class ConstantInputTestCase(TestCase):
@@ -54,16 +37,16 @@ class ConstantInputTestCase(TestCase):
             self.known_assets,
             self.adjustments,
             constants={
-                SomeDataSet.foo: 1,
-                SomeDataSet.bar: 2,
-                SomeDataSet.buzz: 3,
-            }
+                # Every day, assume every stock starts at 2, goes down to 1,
+                # goes up to 4, and finishes at 3.
+                USEquityPricing.low: 1,
+                USEquityPricing.open: 2,
+                USEquityPricing.close: 3,
+                USEquityPricing.high: 4,
+            },
         )
         self.dates = date_range('2014-01-01', '2014-02-01', freq='D')
         self.engine = SimpleFFCEngine(self.loader, self.dates)
-
-    def tearDown(self):
-        pass
 
     def test_single_factor(self):
 
@@ -89,14 +72,14 @@ class ConstantInputTestCase(TestCase):
 
         self.assertEqual(
             set(results.keys()),
-            {factor, SomeDataSet.foo, SomeDataSet.bar},
+            {factor, USEquityPricing.open, USEquityPricing.close},
         )
 
-        for window in results[SomeDataSet.foo].traverse(num_dates):
-            assert_array_equal(window, full(shape, 1))
-
-        for window in results[SomeDataSet.bar].traverse(num_dates):
+        for window in results[USEquityPricing.open].traverse(num_dates):
             assert_array_equal(window, full(shape, 2))
+
+        for window in results[USEquityPricing.close].traverse(num_dates):
+            assert_array_equal(window, full(shape, 3))
 
         assert_array_equal(
             results[factor],
@@ -110,16 +93,30 @@ class ConstantInputTestCase(TestCase):
         dates = self.dates[10:10 + num_dates]
         short_factor = SumDifference(window_length=3)
         long_factor = SumDifference(window_length=5)
+        high_factor = SumDifference(
+            window_length=3,
+            inputs=[USEquityPricing.open, USEquityPricing.high],
+        )
 
         engine.add_factor(short_factor)
         engine.add_factor(long_factor)
+        engine.add_factor(high_factor)
         engine.freeze()
 
-        for input_ in short_factor.inputs:
-            self.assertEqual(
-                engine.extra_row_count(input_),
-                long_factor.window_length - 1,
-            )
+        # open and close should get extended by long_factor
+        self.assertEqual(
+            engine.extra_row_count(USEquityPricing.open),
+            long_factor.window_length - 1,
+        )
+        self.assertEqual(
+            engine.extra_row_count(USEquityPricing.close),
+            long_factor.window_length - 1,
+        )
+        # high should not get extended
+        self.assertEqual(
+            engine.extra_row_count(USEquityPricing.high),
+            high_factor.window_length - 1,
+        )
 
         results = engine.compute_chunk(
             dates[0],
@@ -129,11 +126,27 @@ class ConstantInputTestCase(TestCase):
 
         self.assertEqual(
             set(results.keys()),
-            {short_factor, long_factor, SomeDataSet.foo, SomeDataSet.bar},
+            {
+                short_factor,
+                long_factor,
+                high_factor,
+                USEquityPricing.open,
+                USEquityPricing.close,
+                USEquityPricing.high,
+            },
         )
 
-        for factor in (short_factor, long_factor):
-            assert_array_equal(
-                results[factor],
-                full(shape, -factor.window_length),
-            )
+        # row-wise sum over an array whose values are all (1 - 2)
+        assert_array_equal(
+            results[short_factor],
+            full(shape, -short_factor.window_length),
+        )
+        assert_array_equal(
+            results[long_factor],
+            full(shape, -long_factor.window_length),
+        )
+        # row-wise sum over an array whose values are all (1 - 3)
+        assert_array_equal(
+            results[high_factor],
+            full(shape, -2 * high_factor.window_length),
+        )
