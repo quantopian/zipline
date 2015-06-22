c101e8239965c54799a32a7d61e85d123b77f168
Richard Frank <rich@quantopian.com>
2015-06-22 09:44:38 -0400

    BLD: Pass args to ordered_pip through to pip
    This way, we can specify an index-url, for example.

--- etc/ordered_pip.sh
+++ etc/ordered_pip.sh
@@ -4,7 +4,8 @@ a=0
 while read line
 do     
     if [[ -n "$line" && "$line" != \#* ]] ; then
-        pip install $line
+        # forward to pip any args after the reqs filename
+        pip install $line "${@:2}"
         ((a = a + 1))
     fi
 done < $1
