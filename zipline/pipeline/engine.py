3b8a37236ed3004ef4edc2fa3336f93fc667976d
llllllllll <joe@quantopian.com>
2015-10-02 22:19:53 -0400

    ENH: allows any iterator to be the return of load_adjusted_arrays

--- zipline/pipeline/engine.py
+++ zipline/pipeline/engine.py
@@ -339,9 +339,9 @@ class SimplePipelineEngine(object):
                     key=lambda t: t.dataset
                 )
                 loader = get_loader(term)
-                loaded = loader.load_adjusted_array(
+                loaded = tuple(loader.load_adjusted_array(
                     to_load, mask_dates, assets, mask,
-                )
+                ))
                 assert len(to_load) == len(loaded)
                 for loaded_term, adj_array in zip_longest(to_load, loaded):
                     workspace[loaded_term] = adj_array
