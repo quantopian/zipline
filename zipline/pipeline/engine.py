9674494845938bc716c2434d28dcec9a02a7ec59
Richard Frank <rich@quantopian.com>
2015-10-09 23:55:15 -0400

    MAINT: Renamed loader_dispatch to get_loader
    Now it raises a KeyError instead of returning None,
    if loader not found.

--- zipline/pipeline/engine.py
+++ zipline/pipeline/engine.py
@@ -93,15 +93,15 @@ class SimplePipelineEngine(object):
         which assets are in the top-level universe at any point in time.
     """
     __slots__ = [
-        '_loader_dispatch',
+        '_get_loader',
         '_calendar',
         '_finder',
         '_root_mask_term',
         '__weakref__',
     ]
 
-    def __init__(self, loader_dispatch, calendar, asset_finder):
-        self._loader_dispatch = loader_dispatch
+    def __init__(self, get_loader, calendar, asset_finder):
+        self._get_loader = get_loader
         self._calendar = calendar
         self._finder = asset_finder
         self._root_mask_term = AssetExists()
@@ -275,14 +275,11 @@ class SimplePipelineEngine(object):
             out.append(input_data)
         return out
 
-    def loader_dispatch(self, term):
+    def get_loader(self, term):
         if term is AssetExists():
             return None
 
-        loader = self._loader_dispatch(term)
-        if loader is None:
-            raise ValueError("Couldn't find loader for %s" % term)
-        return loader
+        return self._get_loader(term)
 
     def compute_chunk(self, graph, dates, assets, initial_workspace):
         """
@@ -308,14 +305,14 @@ class SimplePipelineEngine(object):
             Dictionary mapping requested results to outputs.
         """
         self._validate_compute_chunk_params(dates, assets, initial_workspace)
-        loader_dispatch = self.loader_dispatch
+        get_loader = self.get_loader
 
         # Copy the supplied initial workspace so we don't mutate it in place.
         workspace = initial_workspace.copy()
 
         # If atomic terms share the same loader and extra_rows, load them all
         # together.
-        atomic_group_key = juxt(loader_dispatch, getitem(graph.extra_rows))
+        atomic_group_key = juxt(get_loader, getitem(graph.extra_rows))
         atomic_groups = groupby(atomic_group_key, graph.atomic_terms)
 
         for term in graph.ordered():
@@ -337,7 +334,7 @@ class SimplePipelineEngine(object):
                     atomic_groups[atomic_group_key(term)],
                     key=lambda t: t.dataset
                 )
-                loader = loader_dispatch(term)
+                loader = get_loader(term)
                 loaded = loader.load_adjusted_array(
                     to_load, mask_dates, assets, mask,
                 )
