ebc87d15045d5b4b508b71dd6b699446aec0ff82
llllllllll <joe@quantopian.com>
2015-11-06 18:54:34 -0500

    BLD: fix sdist

--- setup.py
+++ setup.py
@@ -192,7 +192,7 @@ pre_setup()
 
 setup(
     name='zipline',
-    version='0.8.0',
+    version='0.8.2',
     description='A backtester for financial algorithms.',
     author='Quantopian Inc.',
     author_email='opensource@quantopian.com',
