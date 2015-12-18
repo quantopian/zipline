022552ee09cd7b66e618a29b78ab38f8bc01ee37
Eddie Hebert <ehebert@quantopian.com>
2015-12-18 11:22:07 -0500

    Tighten diff with master.

--- zipline/finance/blotter.py
+++ zipline/finance/blotter.py
@@ -73,6 +73,7 @@ class Blotter(object):
         self.current_dt = dt
 
     def order(self, sid, amount, style, order_id=None):
+
         # something could be done with amount to further divide
         # between buy by share count OR buy shares up to a dollar amount
         # numeric == share count  AND  "$dollar.cents" == cost amount
