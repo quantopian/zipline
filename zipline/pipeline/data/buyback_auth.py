534f820a9128a19f3a03ef9ea252e4c1a9777d85
Maya Tydykov <mtydykov@quantopian.com>
2016-01-15 16:57:51 -0500

    ENH: add buyback_auth dataset.

--- zipline/pipeline/data/buyback_auth.py
+++ zipline/pipeline/data/buyback_auth.py
@@ -1,1 +1,17 @@
-__author__ = 'mtydykov'
+"""
+Dataset representing dates of upcoming earnings.
+"""
+from zipline.utils.numpy_utils import datetime64ns_dtype, float64_dtype
+
+from .dataset import Column, DataSet
+
+
+class BuybackAuthorizations(DataSet):
+    """
+    Dataset representing dates of recently announced buyback authorization.
+    """
+    previous_buyback_value = Column(float64_dtype)
+    previous_buyback_share_count = Column(float64_dtype)
+    previous_buyback_value_announcement = Column(datetime64ns_dtype)
+    previous_buyback_share_count_announcement = Column(datetime64ns_dtype)
+
