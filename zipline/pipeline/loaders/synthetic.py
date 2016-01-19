22fdc910e414ab231b9e42afbd88dd9091b99bcf
Scott Sanderson <ssanderson@quantopian.com>
2016-01-18 20:50:11 -0500

    DEV: Add EyeLoader.

--- zipline/pipeline/loaders/synthetic.py
+++ zipline/pipeline/loaders/synthetic.py
@@ -6,6 +6,7 @@ from bcolz import ctable
 from numpy import (
     arange,
     array,
+    eye,
     float64,
     full,
     iinfo,
@@ -84,6 +85,29 @@ class PrecomputedLoader(PipelineLoader):
                 loader.load_adjusted_array([col], dates, assets, mask)
             )
         return out
+
+
+class EyeLoader(PrecomputedLoader):
+    """
+    A PrecomputedLoader that emits arrays containing 1s on the diagonal and 0s
+    elsewhere.
+
+    Parameters
+    ----------
+    columns : list[BoundColumn]
+        Columns that this loader should know about.
+    dates : iterable[datetime-like]
+        Same as PrecomputedLoader.
+    sids : iterable[int-like]
+        Same as PrecomputedLoader
+    """
+    def __init__(self, columns, dates, sids):
+        shape = (len(dates), len(sids))
+        super(EyeLoader, self).__init__(
+            {column: eye(shape, dtype=column.dtype) for column in columns},
+            dates,
+            sids,
+        )
 
 
 class SyntheticDailyBarWriter(BcolzDailyBarWriter):
