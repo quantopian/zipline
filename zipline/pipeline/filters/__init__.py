e6361ae48bca7344d1d0d5692b2801f3b4715034
Scott Sanderson <ssanderson@quantopian.com>
2016-03-17 15:54:51 -0400

    MAINT: Re-export CustomFilter and CustomClassifier.

--- zipline/pipeline/filters/__init__.py
+++ zipline/pipeline/filters/__init__.py
@@ -1,7 +1,14 @@
-from .filter import Filter, NumExprFilter, NullFilter, PercentileFilter
+from .filter import (
+    CustomFilter,
+    Filter,
+    NumExprFilter,
+    NullFilter,
+    PercentileFilter,
+)
 from .latest import Latest
 
 __all__ = [
+    'CustomFilter',
     'Filter',
     'Latest',
     'NumExprFilter',
