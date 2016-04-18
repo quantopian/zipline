872b236bedd8ad476d1bf8032b2078f46ea57df5
John Ricklefs <jricklefs@quantopian.com>
2016-04-18 10:51:56 -0400

    ENH: Make arena configurable for SimulationParameters

--- zipline/finance/trading.py
+++ zipline/finance/trading.py
@@ -425,7 +425,8 @@ class SimulationParameters(object):
                  capital_base=10e3,
                  emission_rate='daily',
                  data_frequency='daily',
-                 env=None):
+                 env=None,
+                 arena='backtest'):
 
         self.period_start = period_start
         self.period_end = period_end
@@ -435,7 +436,7 @@ class SimulationParameters(object):
         self.data_frequency = data_frequency
 
         # copied to algorithm's environment for runtime access
-        self.arena = 'backtest'
+        self.arena = arena
 
         if env is not None:
             self.update_internal_from_env(env=env)
