9a59cd064bb8d6246c8f0c520678e310984e66d0
Richard Frank <rich@quantopian.com>
2016-04-20 15:44:51 -0400

    BLD: Download miniconda over SSL

--- ci/appveyor/install.ps1
+++ ci/appveyor/install.ps1
@@ -2,7 +2,7 @@
 # Authors: Olivier Grisel, Jonathan Helmus and Kyle Kastner, Robert McGibbon
 # License: CC0 1.0 Universal: http://creativecommons.org/publicdomain/zero/1.0/
 
-$MINICONDA_URL = "http://repo.continuum.io/miniconda/"
+$MINICONDA_URL = "https://repo.continuum.io/miniconda/"
 
 
 function DownloadMiniconda ($python_version, $platform_suffix) {
