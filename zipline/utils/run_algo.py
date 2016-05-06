76237c587dc1514f8a755384370e9e2fe95f921f
jfkirk <james.f.kirk@gmail.com>
2016-05-06 15:40:18 -0400

    BUG: Adds schedule arg to run_algo

--- zipline/utils/run_algo.py
+++ zipline/utils/run_algo.py
@@ -20,6 +20,7 @@ from zipline.data.data_portal import DataPortal
 from zipline.finance.trading import TradingEnvironment
 from zipline.pipeline.data import USEquityPricing
 from zipline.pipeline.loaders import USEquityPricingLoader
+from zipline.utils.calendars import default_nyse_schedule
 import zipline.utils.paths as pth
 
 
@@ -129,7 +130,7 @@ def _run(handle_data,
             )
         env = TradingEnvironment(asset_db_path=connstr)
         data = DataPortal(
-            env,
+            env, default_nyse_schedule,
             first_trading_day=bundle_data.minute_bar_reader.first_trading_day,
             equity_minute_reader=bundle_data.minute_bar_reader,
             equity_daily_reader=bundle_data.daily_bar_reader,
