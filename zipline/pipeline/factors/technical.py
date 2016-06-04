0f1c08024ab0b6b1519f71df55e86a056270df0c
Joe Jevnik <joe@quantopian.com>
2016-06-04 17:57:44 -0400

    ENH: Adds the ichimoku cloud factor

--- zipline/pipeline/factors/technical.py
+++ zipline/pipeline/factors/technical.py
@@ -445,8 +445,7 @@ class BollingerBands(CustomFactor):
 class Aroon(CustomFactor):
     """
     Aroon technical indicator.
-    https://www.fidelity.com/learning-center/trading-investing/technical
-    -analysis/technical-indicator-guide/aroon-indicator
+    https://www.fidelity.com/learning-center/trading-investing/technical-analysis/technical-indicator-guide/aroon-indicator  # noqa
 
     **Defaults Inputs:** USEquityPricing.low, USEquityPricing.high
 
@@ -521,3 +520,73 @@ class FastStochasticOscillator(CustomFactor):
             global_dict={},
             out=out,
         )
+
+
+class IchimokuKinkoHyo(CustomFactor):
+    """Compute the various metrics for the Ichimoku Kinko Hyo (Ichimoku Cloud).
+    http://stockcharts.com/school/doku.php?id=chart_school:technical_indicators:ichimoku_cloud  # noqa
+
+    **Default Inputs:** :data:`zipline.pipeline.data.USEquityPricing.high`
+                        :data:`zipline.pipeline.data.USEquityPricing.low`
+                        :data:`zipline.pipeline.data.USEquityPricing.close`
+    **Default Window Length:** 52
+
+    Parameters
+    ----------
+    window_length : int > 0
+        The length the the window for the senkou span b.
+    tenkan_sen_length : int >= 0, <= window_length
+        The length of the window for the tenkan-sen.
+    kijun_sen_length : int >= 0, <= window_length
+        The length of the window for the kijou-sen.
+    chikou_span_length : int >= 0, <= window_length
+        The lag for the chikou span.
+    """
+
+    params = {
+        'tenkan_sen_length': 9,
+        'kijun_sen_length': 26,
+        'chikou_span_length': 26,
+    }
+    inputs = USEquityPricing.high, USEquityPricing.close
+    outputs = (
+        'tenkan_sen',
+        'kijun_sen',
+        'senkou_span_a',
+        'senkou_span_b',
+        'chikou_span',
+    )
+    window_length = 52
+
+    def _validate(self):
+        super(IchimokuKinkoHyo, self)._validate()
+        for k, v in self.params.items():
+            if v > self.window_length:
+                raise ValueError(
+                    '%s must be <= the window_length: %s > %s' % (
+                        k, v, self.window_length,
+                    ),
+                )
+
+    def compute(self,
+                today,
+                assets,
+                out,
+                high,
+                low,
+                close,
+                tenkan_sen_length,
+                kijun_sen_length,
+                chikou_span_length):
+
+        out.tenkan_sen = tenkan_sen = (
+            high[-tenkan_sen_length:].max(axis=0) +
+            low[-tenkan_sen_length:].min(axis=0)
+        ) / 2
+        out.kijun_sen = kijun_sen = (
+            high[-kijun_sen_length:].max(axis=0) +
+            low[-kijun_sen_length:].min(axis=0)
+        ) / 2
+        out.senkou_span_a = (tenkan_sen + kijun_sen) / 2
+        out.senkou_span_b = (high.max(axis=0) + low.min(axis=0)) / 2
+        out.chikou_span = close[chikou_span_length]
