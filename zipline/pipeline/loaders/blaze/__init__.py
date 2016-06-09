bc302beec90c67646db6203dfe30ebf84598f01d
Scott Sanderson <ssanderson@quantopian.com>
2016-06-09 01:54:13 -0400

    MAINT: Rework event datasets.
    - Refactored EventsLoader and BlazeEventsLoader to not require a
      subclass per dataset.  Instead, you now pass a map from columns to
      event fields directly to the EventsLoader constructor.
    
    - Removed a large number of Quantopian-specific datasets and associated
      tests.
    
    - Rewrote the core logic of EventsLoader and BlazeEventsLoader to share
      index calculations across multiple requested columns.
    
    - Fixed a bug where event fields were incorrectly forward-filled when
      null values were present in an event.

--- zipline/pipeline/loaders/blaze/__init__.py
+++ zipline/pipeline/loaders/blaze/__init__.py
@@ -1,30 +1,11 @@
-from ._13d_filings import Blaze_13DFilingsLoader
-
-from .buyback_auth import BlazeBuybackAuthorizationsLoader
 from .core import (
     BlazeLoader,
     NoDeltasWarning,
     from_blaze,
     global_loader,
 )
-from .dividends import (
-    BlazeDividendsByAnnouncementDateLoader,
-    BlazeDividendsByExDateLoader,
-    BlazeDividendsByPayDateLoader
-)
-from .earnings import (
-    BlazeEarningsCalendarLoader,
-)
-from .consensus_estimates import BlazeConsensusEstimatesLoader
 
 __all__ = (
-    'Blaze_13DFilingsLoader',
-    'BlazeBuybackAuthorizationsLoader',
-    'BlazeDividendsByAnnouncementDateLoader',
-    'BlazeConsensusEstimatesLoader',
-    'BlazeDividendsByExDateLoader',
-    'BlazeDividendsByPayDateLoader',
-    'BlazeEarningsCalendarLoader',
     'BlazeLoader',
     'from_blaze',
     'global_loader',
