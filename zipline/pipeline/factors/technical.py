d1d2fe7d935124f168b38986c3d160bef2a1a0b4
Samuel Woo <swacademics@gmail.com>
2016-06-15 10:35:47 -0400

    ENH: Vectorized compute method for LinearWeightedMovingAverage; added extra test to TestLinearWeightedMovingAverage

--- zipline/pipeline/factors/technical.py
+++ zipline/pipeline/factors/technical.py
@@ -133,10 +133,14 @@ class LinearWeightedMovingAverage(CustomFactor, SingleInputMixin):
 
         # Compute weighted averages
 <<<<<<< HEAD
+<<<<<<< HEAD
         out[:] = np.nansum(weighted_data, axis=0) / normalizer
 =======
         out = np.nansum(weighted_data, axis=0) / normalizer
 >>>>>>> ENH: Adds LinearWeightedMovingAverage Factor
+=======
+        out[:] = np.nansum(weighted_data, axis=0) / normalizer
+>>>>>>> ENH: Vectorized compute method for LinearWeightedMovingAverage; added extra test to TestLinearWeightedMovingAverage
 
 
 class WeightedAverageValue(CustomFactor):
