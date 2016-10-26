810ee7f2af2ef01720bfef0fbafc80e85985d301
Paul Sutherland <psutherland@quantopian.com>
2016-10-26 18:45:44 -0400

    DEV delete old *.c and *.so files with rebuild-cython.sh for Darwin/OSX

--- etc/rebuild-cython.sh
+++ etc/rebuild-cython.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
-find zipline tests -regex '.*\.\(c\|so\)' -exec rm {} +
+
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    find -E zipline tests -regex '.*\.(c|so)' -exec rm {} +
+else
+    find zipline tests -regex '.*\.\(c\|so\)' -exec rm {} +
+fi
 python setup.py build_ext --inplace
