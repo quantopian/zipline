3f02154f65031a03f6fc3ef35a099cf989591fff
Richard Frank <rich@quantopian.com>
2017-02-22 14:55:20 -0500

    Revert "Moved short script into meta.yaml"
    This reverts commit b6a59edeaf91d4a93edb2cdf5a1fe7e23ad36c8a.

---
+++ conda/zipline/bld.bat
@@ -0,0 +1,8 @@
+"%PYTHON%" setup.py install --single-version-externally-managed --record=record.txt
+if errorlevel 1 exit 1
+
+:: Add more build steps here, if they are necessary.
+
+:: See
+:: http://docs.continuum.io/conda/build.html
+:: for a list of environment variables that are set during the build process.
