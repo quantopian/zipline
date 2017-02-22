57f8eb3bf5831616af756b3eef02ee6d76842106
Richard Frank <rich@quantopian.com>
2017-02-22 12:58:16 -0500

    workaround for setuptools bug

--- setup.py
+++ setup.py
@@ -28,7 +28,10 @@ from setuptools import (
     Extension,
     find_packages,
     setup,
+    dist,
 )
+from setuptools.command.install import install
+from setuptools.command.install_scripts import install_scripts
 
 import versioneer
 
@@ -277,11 +280,70 @@ conditional_arguments = {
     'setup_requires' if not conda_build else 'build_requires': setup_requires,
 }
 
+
+#-----------------------------------------------------------------------------
+# Hack to overcome pip/setuptools problem on Win 10.  See:
+#   https://github.com/tomduck/pandoc-eqnos/issues/6
+#   https://github.com/pypa/pip/issues/2783
+# Note that cmdclass must be be hooked into setup().
+
+# Custom install command class for setup()
+class custom_install(install):
+    """Ensures setuptools uses custom install_scripts."""
+    def run(self):
+        install.run(self)
+
+# Custom install_scripts command class for setup()
+class install_scripts_quoted_shebang(install_scripts):
+    """Ensure there are quotes around shebang paths with spaces."""
+    def write_script(self, script_name, contents, mode="t", *ignored):
+        shebang = str(contents.splitlines()[0])
+        if shebang.startswith('#!') and ' ' in shebang[2:].strip() \
+          and '"' not in shebang:
+            quoted_shebang = '#!"%s"' % shebang[2:].strip()
+            contents = contents.replace(shebang, quoted_shebang)
+            install_scripts.write_script(self, script_name, contents, mode, *ignored)
+
+# The custom command classes only need to be used on Windows machines
+if os.name == 'nt':
+    cmdclass = {'install': custom_install,
+                'install_scripts': install_scripts_quoted_shebang},
+
+    # Below is another hack to overcome a separate bug.  The
+    # dist.Distribution.cmdclass dict should not be stored in a length-1 list.
+
+    # Save the original method
+    dist.Distribution._get_command_class = dist.Distribution.get_command_class
+
+    # Define a new method that repairs self.cmdclass if needed
+    def get_command_class(self, command):
+        """Pluggable version of get_command_class()"""
+        try:
+            # See if the original behaviour works
+            return dist.Distribution._get_command_class(self, command)
+        except TypeError:
+            # If self.cmdclass is the problem, fix it up
+            if type(self.cmdclass) is tuple and type(self.cmdclass[0]) is dict:
+                self.cmdclass = self.cmdclass[0]
+                return dist.Distribution._get_command_class(self, command)
+            else:
+                # Something else went wrong
+                raise
+
+    # Hook in the new method
+    dist.Distribution.get_command_class = get_command_class
+
+else:
+    cmdclass = {}
+
+
+cmdclass.update(versioneer.get_cmdclass())
+
 setup(
     name='zipline',
     url="http://zipline.io",
     version=versioneer.get_version(),
-    cmdclass=LazyBuildExtCommandClass(versioneer.get_cmdclass()),
+    cmdclass=LazyBuildExtCommandClass(cmdclass),
     description='A backtester for financial algorithms.',
     entry_points={
         'console_scripts': [
