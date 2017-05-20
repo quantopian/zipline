146874dff1d00c50f135a39d98e8657aadcf2644
Ed Bartosh <bartosh@gmail.com>
2017-05-20 19:11:30 +0300

    get benchmark data from Google finance
    Yahoo finance stopped to provide ^GSPC data that was
    used as a benchmark in zipline.
    
    Changed benchmark to SPY.
    Got benchmark data from Google Finance.
    
    Thanks to Peter Harrington for the implementaion.
    
    Signed-off-by: Ed Bartosh <bartosh@gmail.com>

--- zipline/data/benchmarks.py
+++ zipline/data/benchmarks.py
@@ -12,50 +12,17 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-import pandas as pd
 
-from six.moves.urllib_parse import urlencode
-
-
-def format_yahoo_index_url(symbol, start_date, end_date):
-    """
-    Format a URL for querying Yahoo Finance for Index data.
-    """
-    return (
-        'https://ichart.finance.yahoo.com/table.csv?' + urlencode({
-            's': symbol,
-            # start_date month, zero indexed
-            'a': start_date.month - 1,
-            # start_date day
-            'b': start_date.day,
-            # start_date year
-            'c': start_date.year,
-            # end_date month, zero indexed
-            'd': end_date.month - 1,
-            # end_date day
-            'e': end_date.day,
-            # end_date year
-            'f': end_date.year,
-            # daily frequency
-            'g': 'd',
-        })
-    )
-
+import pandas_datareader.data as web
 
 def get_benchmark_returns(symbol, start_date, end_date):
     """
-    Get a Series of benchmark returns from Yahoo.
+    Get a Series of benchmark returns from Google finance.
 
     Returns a Series with returns from (start_date, end_date].
 
     start_date is **not** included because we need the close from day N - 1 to
     compute the returns for day N.
     """
-    return pd.read_csv(
-        format_yahoo_index_url(symbol, start_date, end_date),
-        parse_dates=['Date'],
-        index_col='Date',
-        usecols=["Adj Close", "Date"],
-        squeeze=True,  # squeeze tells pandas to make this a Series
-                       # instead of a 1-column DataFrame
-    ).sort_index().tz_localize('UTC').pct_change(1).iloc[1:]
+    df = web.DataReader(symbol, 'google', start_date, end_date)
+    return df["Close"].sort_index().tz_localize('UTC').pct_change(1).iloc[1:]
