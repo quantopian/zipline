d39afc99a4504901db267f037f564f9ff28dcfe5
Ed Bartosh <bartosh@gmail.com>
2017-05-21 19:04:50 +0300

    fill missed benchmark data
    Google Finance price data for SPY missing some dates.
    This causes algo breakages like this:
        KeyError: 'the label [2012-02-02 00:00:00+00:00] is not in the [index]'
    
    Used session data from NYSE calendar to fill missing SPY price data.
    
    Signed-off-by: Ed Bartosh <bartosh@gmail.com>

--- zipline/data/benchmarks.py
+++ zipline/data/benchmarks.py
@@ -13,6 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from zipline.utils.calendars import get_calendar
 import pandas_datareader.data as web
 
 def get_benchmark_returns(symbol, start_date, end_date):
@@ -24,5 +25,14 @@ def get_benchmark_returns(symbol, start_date, end_date):
     start_date is **not** included because we need the close from day N - 1 to
     compute the returns for day N.
     """
-    df = web.DataReader(symbol, 'google', start_date, end_date)
-    return df["Close"].sort_index().tz_localize('UTC').pct_change(1).iloc[1:]
+    df = web.DataReader(symbol, 'google', start_date, end_date) 
+    df.index = df.index.tz_localize('UTC')
+
+    calendar = get_calendar("NYSE")
+    start_index = calendar.all_sessions.searchsorted(start_date)
+    end_index = calendar.all_sessions.searchsorted(end_date)
+
+    # fill price data for missing dates
+    df = df["Close"].reindex(calendar.all_sessions[start_index:end_index], method='ffill')
+
+    return df.pct_change(1).iloc[1:]
