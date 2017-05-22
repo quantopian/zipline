4262e9fa6b67d56c33202ce05d6a1128338ebf7f
Tibor Kiss <tibor.kiss@gmail.com>
2017-05-22 20:50:26 +0200

    Fix ib_broker import from brokers

--- zipline/gens/brokers/__init__.py
+++ zipline/gens/brokers/__init__.py
@@ -11,6 +11,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from ib_broker import IBBroker
+from .ib_broker import IBBroker
 
 __all__ = ["IBBroker"]
