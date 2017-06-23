1f7c51b0041a3c17edb400754b923cfcafb460b8
Tibor Kiss <tibor.kiss@gmail.com>
2017-06-23 06:59:28 +0200

    Live: Fix data.history() call's date range

--- zipline/data/data_portal_live.py
+++ zipline/data/data_portal_live.py
@@ -11,6 +11,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import pandas as pd
+
 from zipline.data.data_portal import DataPortal
 
 from logbook import Logger
@@ -22,6 +24,29 @@ class DataPortalLive(DataPortal):
     def __init__(self, broker, *args, **kwargs):
         self.broker = broker
         super(DataPortalLive, self).__init__(*args, **kwargs)
+
+    def get_history_window(self,
+                           assets,
+                           end_dt,
+                           bar_count,
+                           frequency,
+                           field,
+                           data_frequency,
+                           ffill=True):
+        history_window = super(self.__class__, self).get_history_window(
+            assets,
+            end_dt,
+            bar_count,
+            frequency,
+            field,
+            data_frequency,
+            ffill)
+
+        # The returned dataframe contains today's value as a NaN because
+        # end_dt points to the current wall clock. We drop today's
+        # value to be in sync with the simulation's behavior.
+        today = pd.to_datetime('now').date()
+        return history_window[history_window.index.date != today]
 
     def get_spot_value(self, assets, field, dt, data_frequency):
         return self.broker.get_spot_value(assets, field, dt, data_frequency)
