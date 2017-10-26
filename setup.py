deb8c762d3eafd2fc848c85c9fc100433013c3bb
Scott Sanderson <ssanderson@quantopian.com>
2017-10-26 13:17:33 -0400

    EXPERIMENT: What happens if we install from vendored numpy headers?

--- setup.py
+++ setup.py
@@ -33,49 +33,10 @@ from setuptools import (
 import versioneer
 
 
-class LazyBuildExtCommandClass(dict):
-    """
-    Lazy command class that defers operations requiring Cython and numpy until
-    they've actually been downloaded and installed by setup_requires.
-    """
-    def __contains__(self, key):
-        return (
-            key == 'build_ext'
-            or super(LazyBuildExtCommandClass, self).__contains__(key)
-        )
+from functools import partial
 
-    def __setitem__(self, key, value):
-        if key == 'build_ext':
-            raise AssertionError("build_ext overridden!")
-        super(LazyBuildExtCommandClass, self).__setitem__(key, value)
-
-    def __getitem__(self, key):
-        if key != 'build_ext':
-            return super(LazyBuildExtCommandClass, self).__getitem__(key)
-
-        from Cython.Distutils import build_ext as cython_build_ext
-        import numpy
-
-        # Cython_build_ext isn't a new-style class in Py2.
-        class build_ext(cython_build_ext, object):
-            """
-            Custom build_ext command that lazily adds numpy's include_dir to
-            extensions.
-            """
-            def build_extensions(self):
-                """
-                Lazily append numpy's include directory to Extension includes.
-
-                This is done here rather than at module scope because setup.py
-                may be run before numpy has been installed, in which case
-                importing numpy and calling `numpy.get_include()` will fail.
-                """
-                numpy_incl = numpy.get_include()
-                for ext in self.extensions:
-                    ext.include_dirs.append(numpy_incl)
-
-                super(build_ext, self).build_extensions()
-        return build_ext
+vendored_numpy = join(dirname(abspath(__file__)), 'vendored_numpy_headers')
+Extension = partial(Extension, include_dirs=[vendored_numpy])
 
 
 def window_specialization(typename):
@@ -268,6 +229,7 @@ def setup_requirements(requirements_path, module_names, strict_bounds,
         )
     return module_lines
 
+
 conda_build = os.path.basename(sys.argv[0]) in ('conda-build',  # unix
                                                 'conda-build-script.py')  # win
 
@@ -286,7 +248,7 @@ setup(
     name='zipline',
     url="http://zipline.io",
     version=versioneer.get_version(),
-    cmdclass=LazyBuildExtCommandClass(versioneer.get_cmdclass()),
+    cmdclass=versioneer.get_cmdclass(),
     description='A backtester for financial algorithms.',
     entry_points={
         'console_scripts': [
