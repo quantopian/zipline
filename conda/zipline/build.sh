fbc76ca0f36bcced656cd80a22cad9da0215ba88
Richard Frank <rich@quantopian.com>
2017-11-07 13:51:34 -0500

    BLD: Updated recipes for newer conda build
    setuptools-scm, bcolz, zipline

--- conda/zipline/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-$PYTHON setup.py install --single-version-externally-managed --record=record.txt
-
-# Add more build steps here, if they are necessary.
-
-# See
-# http://docs.continuum.io/conda/build.html
-# for a list of environment variables that are set during the build process.
