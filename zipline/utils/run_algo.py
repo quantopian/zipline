7c70d58ab276140d41e3d9af51930920bbeb4315
Luke Van Seters <lukevanseters@gmail.com>
2018-01-14 16:14:58 -0500

    ENH: Make emission_rate configurable in run_algorithm

--- zipline/utils/run_algo.py
+++ zipline/utils/run_algo.py
@@ -54,6 +54,7 @@ def _run(handle_data,
          algotext,
          defines,
          data_frequency,
+         emission_rate,
          capital_base,
          data,
          bundle,
@@ -170,6 +171,7 @@ def _run(handle_data,
             end=end,
             capital_base=capital_base,
             data_frequency=data_frequency,
+            emission_rate=emission_rate,
             trading_calendar=trading_calendar,
         ),
         **{
@@ -255,6 +257,7 @@ def run_algorithm(start,
                   before_trading_start=None,
                   analyze=None,
                   data_frequency='daily',
+                  emission_rate='daily',
                   data=None,
                   bundle=None,
                   bundle_timestamp=None,
@@ -290,6 +293,8 @@ def run_algorithm(start,
         performance data.
     data_frequency : {'daily', 'minute'}, optional
         The data frequency to run the algorithm at.
+    emission_rate : {'daily', 'minute'}, optional
+        The emission rate to run the algorithm at.
     data : pd.DataFrame, pd.Panel, or DataPortal, optional
         The ohlcv data to run the backtest with.
         This argument is mutually exclusive with:
@@ -358,6 +363,7 @@ def run_algorithm(start,
         algotext=None,
         defines=(),
         data_frequency=data_frequency,
+        emission_rate=emission_rate,
         capital_base=capital_base,
         data=data,
         bundle=bundle,
