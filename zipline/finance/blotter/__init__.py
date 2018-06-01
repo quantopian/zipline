152322f78013ddb0daada45ea86d27aa90507647
Jacob Nazarenko <jnazarenko@quantopian.com>
2018-06-01 12:00:52 -0400

    MAINT/STY: Fix various issues from first round of code review

--- zipline/finance/blotter/__init__.py
+++ zipline/finance/blotter/__init__.py
@@ -13,7 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from .simulatedblotter import SimulatedBlotter
+from .simulated_blotter import SimulatedBlotter
 from .core import (
     blotters,
     register,
