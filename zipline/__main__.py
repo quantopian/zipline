be0ce70449e69f98e6d5ec4a621af0005293084e
Jacob Nazarenko <jnazarenko@quantopian.com>
2018-06-01 10:45:02 -0400

    ENH: Add command line option for custom blotter class

--- zipline/__main__.py
+++ zipline/__main__.py
@@ -200,6 +200,12 @@ def ipython_only(option):
     help='The metrics set to use. New metrics sets may be registered in your'
     ' extension.py.',
 )
+@click.option(
+    '--blotter-class',
+    default='default',
+    help='The blotter class to use for running the algorithm. Custom '
+         'blotter classes may be registered in your extension.py.',
+)
 @ipython_only(click.option(
     '--local-namespace/--no-local-namespace',
     is_flag=True,
@@ -221,6 +227,7 @@ def run(ctx,
         trading_calendar,
         print_algo,
         metrics_set,
+        blotter_class,
         local_namespace):
     """Run a backtest for the given algorithm.
     """
@@ -266,6 +273,7 @@ def run(ctx,
         metrics_set=metrics_set,
         local_namespace=local_namespace,
         environ=os.environ,
+        blotter_class=blotter_class
     )
 
     if output == '-':
