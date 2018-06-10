621924765ee5c59f31254fb2f95a540b143afa35
Jacob Nazarenko <jnazarenko@quantopian.com>
2018-06-10 00:12:11 -0400

    ENH/TST: Restructure arg parsing + update/expand tests

--- zipline/extensions.py
+++ zipline/extensions.py
@@ -1,44 +1,45 @@
 import re
 
 
-class ExtensionArgs(object):
+def create_args(args, root):
 
-    def __init__(self, args):
-        self.extension_args = {}
+    extension_args = {}
 
-        for arg in args:
-            self.parse_extension_arg(arg)
+    for arg in args:
+        parse_extension_arg(arg, extension_args)
 
-        for name in sorted(self.extension_args, key=len):
-            path = name.split('.')
-            self.get_namespace(self, path, self.extension_args[name])
+    for name in sorted(extension_args, key=len):
+        path = name.split('.')
+        get_namespace(root, path, extension_args[name])
 
-    def parse_extension_arg(self, arg):
-        match = re.match(r'^(([^\d\W]\w*)(\.[^\d\W]\w*)*)=(.*)$', arg)
-        if match is None:
-            raise ValueError(
-                "invalid extension argument %s, must be in key=value form" %
-                arg)
 
-        name = match.group(1)
-        value = match.group(4)
-        self.extension_args[name] = value
+def parse_extension_arg(arg, arg_dict):
+    match = re.match(r'^(([^\d\W]\w*)(\.[^\d\W]\w*)*)=(.*)$', arg)
+    if match is None:
+        raise ValueError(
+            "invalid extension argument %s, must be in key=value form" %
+            arg)
 
-    def get_namespace(self, obj, path, name):
-        if len(path) == 1:
-            setattr(obj, path[0], name)
+    name = match.group(1)
+    value = match.group(4)
+    arg_dict[name] = value
+
+
+def get_namespace(obj, path, name):
+    if len(path) == 1:
+        setattr(obj, path[0], name)
+    else:
+        if hasattr(obj, path[0]):
+            if type(getattr(obj, path[0])) is str:
+                raise ValueError("Conflicting assignments at namespace"
+                                 " level '%s'" % path[0])
+            get_namespace(getattr(obj, path[0]), path[1:], name)
         else:
-            if hasattr(obj, path[0]):
-                if type(getattr(obj, path[0])) is str:
-                    raise ValueError("Conflicting assignments at namespace"
-                                     " level '%s'" % path[0])
-                self.get_namespace(getattr(obj, path[0]), path[1:], name)
-            else:
-                a = Arg()
-                setattr(obj, path[0], a)
-                self.get_namespace(getattr(obj, path[0]), path[1:], name)
+            a = Namespace()
+            setattr(obj, path[0], a)
+            get_namespace(getattr(obj, path[0]), path[1:], name)
 
 
-class Arg(object):
+class Namespace(object):
 
     pass
