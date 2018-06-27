64ed52de9f012abfe53728cc4995aa4f0bdff246
Scott Sanderson <ssanderson@quantopian.com>
2018-06-27 11:52:01 -0400

    BUG: Fix daily mode futures algorithms.

--- zipline/algorithm.py
+++ zipline/algorithm.py
@@ -491,23 +491,25 @@ class TradingAlgorithm(object):
 
         if self.sim_params.data_frequency == 'minute':
             market_opens = trading_o_and_c['market_open']
-
             minutely_emission = self.sim_params.emission_rate == "minute"
+
+            # The calendar's execution times are the minutes over which we
+            # actually want to run the clock. Typically the execution times
+            # simply adhere to the market open and close times. In the case of
+            # the futures calendar, for example, we only want to simulate over
+            # a subset of the full 24 hour calendar, so the execution times
+            # dictate a market open time of 6:31am US/Eastern and a close of
+            # 5:00pm US/Eastern.
+            execution_opens = \
+                self.trading_calendar.execution_time_from_open(market_opens)
+            execution_closes = \
+                self.trading_calendar.execution_time_from_close(market_closes)
         else:
             # in daily mode, we want to have one bar per session, timestamped
             # as the last minute of the session.
-            market_opens = market_closes
-
-        # The calendar's execution times are the minutes over which we actually
-        # want to run the clock. Typically the execution times simply adhere to
-        # the market open and close times. In the case of the futures calendar,
-        # for example, we only want to simulate over a subset of the full 24
-        # hour calendar, so the execution times dictate a market open time of
-        # 6:31am US/Eastern and a close of 5:00pm US/Eastern.
-        execution_opens = \
-            self.trading_calendar.execution_time_from_open(market_opens)
-        execution_closes = \
-            self.trading_calendar.execution_time_from_close(market_closes)
+            execution_closes = \
+                self.trading_calendar.execution_time_from_close(market_closes)
+            execution_opens = execution_closes
 
         # FIXME generalize these values
         before_trading_start_minutes = days_at_time(
