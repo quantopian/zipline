c56e7bd1be1c4f9b28438944c50dc82ad23f2905
vikram-narayan <vnarayan@quantopian.com>
2018-07-02 16:02:19 -0400

    MAINT: reexport more calendar stuff for backwards compat

--- zipline/utils/calendars.py
+++ zipline/utils/calendars.py
@@ -3,9 +3,12 @@
 from trading_calendars import (
     clear_calendars,
     deregister_calendar,
+    exchange_calendar_nyse,
     get_calendar,
     register_calendar,
     register_calendar_alias,
     register_calendar_type,
+    trading_calendar,
     TradingCalendar,
+    us_futures_calendar
 )
