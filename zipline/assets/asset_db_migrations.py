178ef727ae321f18f0ce8bad1fdef4c2e9ad9549
Joe Jevnik <joe@quantopian.com>
2018-07-09 15:16:17 -0400

    ENH: make db downgrade drop all non-us assets

--- zipline/assets/asset_db_migrations.py
+++ zipline/assets/asset_db_migrations.py
@@ -409,6 +409,8 @@ def _downgrade_v7(op):
             exchanges ex
         on
             eq.exchange == ex.exchange
+        where
+            ex.country_code in ('US', '??')
         """,
     )
     op.drop_table('equities')
