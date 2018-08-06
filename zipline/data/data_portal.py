05092d104de2b95bc55b5d4e1284e2eb14087075
Jacob Nazarenko <jnazarenko@quantopian.com>
2018-08-06 11:09:11 -0400

    A follow-up to the follow-up

--- zipline/data/data_portal.py
+++ zipline/data/data_portal.py
@@ -908,7 +908,8 @@ class DataPortal(object):
                                        end_dt,
                                        field_to_use,
                                        data_frequency):
-        if data_frequency == 'daily' and not self._is_live:
+        if data_frequency == 'daily' and \
+                (not self._is_live or days_for_window[-1] < self._live_day):
             # two cases where we use daily data for the whole range:
             # 1) the history window ends at midnight utc.
             # 2) the last desired day of the window is after the
