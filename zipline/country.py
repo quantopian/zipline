157bff790eaaf09369e4f93846db525abcff6209
Scott Sanderson <ssanderson@quantopian.com>
2018-08-29 14:10:13 -0400

    MAINT: Simplify canonical country codes.

--- zipline/country.py
+++ zipline/country.py
@@ -3,12 +3,16 @@
 from iso3166 import countries_by_name
 
 
+def code(name):
+    return countries_by_name[name].alpha2
+
+
 class CountryCode(object):
     """A simple namespace of iso3166 alpha2 country codes.
     """
-    CANADA = countries_by_name['CANADA'].alpha2
-    GERMANY = countries_by_name['GERMANY'].alpha2
-    UNITED_KINGDOM = countries_by_name[
+    CANADA = code('CANADA')
+    GERMANY = code('GERMANY')
+    UNITED_KINGDOM = code(
         'UNITED KINGDOM OF GREAT BRITAIN AND NORTHERN IRELAND'
-    ].alpha2
-    UNITED_STATES = countries_by_name['UNITED STATES OF AMERICA'].alpha2
+    )
+    UNITED_STATES = code('UNITED STATES OF AMERICA')
