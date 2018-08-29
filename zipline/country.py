72993a3a4e6dfb58116663f56efd1ec1bd05bed9
Scott Sanderson <ssanderson@quantopian.com>
2018-08-29 14:18:08 -0400

    MAINT: Add a bunch more country codes.

--- zipline/country.py
+++ zipline/country.py
@@ -10,8 +10,26 @@ def code(name):
 class CountryCode(object):
     """A simple namespace of iso3166 alpha2 country codes.
     """
+    AUSTRALIA = code('AUSTRALIA')
+    AUSTRIA = code('AUSTRIA')
+    BELGIUM = code('BELGIUM')
     CANADA = code('CANADA')
+    DENMARK = code('DENMARK')
+    FINLAND = code('FINLAND')
+    FRANCE = code('FRANCE')
     GERMANY = code('GERMANY')
+    HONG_KONG = code('HONG KONG')
+    IRELAND = code('IRELAND')
+    ISRAEL = code('ISRAEL')
+    ITALY = code('ITALY')
+    JAPAN = code('JAPAN')
+    NETHERLANDS = code('NETHERLANDS')
+    NEW_ZEALAND = code('NEW ZEALAND')
+    NORWAY = code('NORWAY')
+    PORTUGAL = code('PORTUGAL')
+    SINGAPORE = code('SINGAPORE')
+    SWEDEN = code('SWEDEN')
+    SWITZERLAND = code('SWITZERLAND')
     UNITED_KINGDOM = code(
         'UNITED KINGDOM OF GREAT BRITAIN AND NORTHERN IRELAND'
     )
