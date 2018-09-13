e987c980f6b244364c2143adb7aeb59f662ffe17
Richard Frank <rich@quantopian.com>
2018-09-13 10:05:15 -0400

    BLD: Cache miniconda dir

--- ci/travis/install_miniconda.sh
+++ ci/travis/install_miniconda.sh
@@ -6,7 +6,11 @@ else
     MINICONDA_OS=Linux
 fi
 
-wget "https://repo.continuum.io/miniconda/Miniconda${CONDA_ROOT_PYTHON_VERSION:0:1}-4.3.30-$MINICONDA_OS-x86_64.sh" -O miniconda.sh
-chmod +x miniconda.sh
-./miniconda.sh -b -p $HOME/miniconda
-export PATH="$HOME/miniconda/bin:$PATH"
+MINICONDA_DIR="$HOME/.cache/miniconda/${CONDA_ROOT_PYTHON_VERSION:0:1}-4.3.30-$MINICONDA_OS"
+
+if [ ! -d "$MINICONDA_DIR" ]; then
+    wget "https://repo.continuum.io/miniconda/Miniconda${CONDA_ROOT_PYTHON_VERSION:0:1}-4.3.30-$MINICONDA_OS-x86_64.sh" -O miniconda.sh
+    chmod +x miniconda.sh
+    ./miniconda.sh -b -p $MINICONDA_DIR
+fi
+export PATH="$MINICONDA_DIR/bin:$PATH"
