be6bc358525e796a1bc2b1d6556ee1847b6f4b02
Richard Frank <rich@quantopian.com>
2018-09-13 10:21:19 -0400

    new approach

--- ci/travis/install_miniconda.sh
+++ ci/travis/install_miniconda.sh
@@ -6,11 +6,12 @@ else
     MINICONDA_OS=Linux
 fi
 
-MINICONDA_DIR="$HOME/.cache/miniconda/${CONDA_ROOT_PYTHON_VERSION:0:1}-4.3.30-$MINICONDA_OS"
+MINICONDA_DL_DIR="$HOME/.cache/miniconda/${CONDA_ROOT_PYTHON_VERSION:0:1}-4.3.30-$MINICONDA_OS"
 
-if [ ! -d "$MINICONDA_DIR" ]; then
+if [ ! -d "$MINICONDA_DL_DIR" ]; then
     wget "https://repo.continuum.io/miniconda/Miniconda${CONDA_ROOT_PYTHON_VERSION:0:1}-4.3.30-$MINICONDA_OS-x86_64.sh" -O miniconda.sh
     chmod +x miniconda.sh
-    ./miniconda.sh -b -p $MINICONDA_DIR
+    cp miniconda.sh $MINICONDA_DL_DIR
+    ./miniconda.sh -b -p $HOME/miniconda
 fi
-export PATH="$MINICONDA_DIR/bin:$PATH"
+export PATH="$HOME/miniconda/bin:$PATH"
