348684aa4f2ca718c85c9171968d9b29d8f65e56
Joe Jevnik <joe@quantopian.com>
2019-02-26 18:35:44 -0500

    BUG: make parameter_space reset instance fixtures between runs

--- zipline/testing/core.py
+++ zipline/testing/core.py
@@ -1200,16 +1200,23 @@ def parameter_space(__fail_fast=_FAIL_FAST_DEFAULT, **params):
         def make_param_sets():
             return product(*(params[name] for name in argnames))
 
+        def clean_f(self, *args, **kwargs):
+            try:
+                f(self, *args, **kwargs)
+            finally:
+                self.tearDown()
+                self.setUp()
+
         if __fail_fast:
             @wraps(f)
             def wrapped(self):
                 for args in make_param_sets():
-                    f(self, *args)
+                    clean_f(self, *args)
             return wrapped
         else:
             @wraps(f)
             def wrapped(*args, **kwargs):
-                subtest(make_param_sets(), *argnames)(f)(*args, **kwargs)
+                subtest(make_param_sets(), *argnames)(clean_f)(*args, **kwargs)
 
         return wrapped
 
