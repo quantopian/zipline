f3223dd11d1de3485bb0069b8eb8f3c0251124fb
Dani Sim <dsim@quantopian.com>
2020-01-27 15:35:24 -0500

    Remove macos, windows runner os for now.

--- ci/actions/overwrite_requirements.sh
+++ ci/actions/overwrite_requirements.sh
@@ -15,10 +15,8 @@ sed_inplace "s/numpy==.*/numpy==$NUMPY_VERSION/" etc/requirements.txt
 sed_inplace "s/pandas==.*/pandas==$PANDAS_VERSION/" etc/requirements.txt
 sed_inplace "s/scipy==.*/scipy==$SCIPY_VERSION/" etc/requirements.txt
 if [ -n matrix.pandas-datareader-version ]; then
-    export PANDAS_DATAREADER_VERSION=${{ matrix.pandas-datareader-version }}
     sed_inplace "s/pandas-datareader==.*/pandas-datareader==$PANDAS_DATAREADER_VERSION/" etc/requirements.txt
 fi
 if [ -n matrix.dask-version ]; then
-    export DASK_VERSION=${{ matrix.dask-version }}
     sed_inplace "s/dask\[dataframe\]==.*/dask\[dataframe\]==$DASK_VERSION/" etc/requirements.txt
 fi
