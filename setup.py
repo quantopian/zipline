597fe916ed78b4dfef928eb119cccfb5827205f6
Scott Sanderson <ssanderson@quantopian.com>
2020-02-11 11:35:16 -0500

    WIP: More ci changes.

--- setup.py
+++ setup.py
@@ -25,57 +25,14 @@ from os.path import (
 )
 from distutils.version import StrictVersion
 from setuptools import (
-    Extension,
     find_packages,
     setup,
+    Extension,
 )
 
+from Cython import cythonize
+
 import versioneer
-
-
-class LazyBuildExtCommandClass(dict):
-    """
-    Lazy command class that defers operations requiring Cython and numpy until
-    they've actually been downloaded and installed by setup_requires.
-    """
-    def __contains__(self, key):
-        return (
-            key == 'build_ext'
-            or super(LazyBuildExtCommandClass, self).__contains__(key)
-        )
-
-    def __setitem__(self, key, value):
-        if key == 'build_ext':
-            raise AssertionError("build_ext overridden!")
-        super(LazyBuildExtCommandClass, self).__setitem__(key, value)
-
-    def __getitem__(self, key):
-        if key != 'build_ext':
-            return super(LazyBuildExtCommandClass, self).__getitem__(key)
-
-        from Cython.Distutils import build_ext as cython_build_ext
-        import numpy
-
-        # Cython_build_ext isn't a new-style class in Py2.
-        class build_ext(cython_build_ext, object):
-            """
-            Custom build_ext command that lazily adds numpy's include_dir to
-            extensions.
-            """
-            def build_extensions(self):
-                """
-                Lazily append numpy's include directory to Extension includes.
-
-                This is done here rather than at module scope because setup.py
-                may be run before numpy has been installed, in which case
-                importing numpy and calling `numpy.get_include()` will fail.
-                """
-                numpy_incl = numpy.get_include()
-                for ext in self.extensions:
-                    ext.include_dirs.append(numpy_incl)
-
-                super(build_ext, self).build_extensions()
-        return build_ext
 
 
 def window_specialization(typename):
@@ -87,7 +44,7 @@ def window_specialization(typename):
     )
 
 
-ext_modules = [
+ext_modules = cythonize([
     Extension('zipline.assets._assets', ['zipline/assets/_assets.pyx']),
     Extension('zipline.assets.continuous_futures',
               ['zipline/assets/continuous_futures.pyx']),
@@ -120,7 +77,7 @@ ext_modules = [
         ['zipline/pipeline/loaders/blaze/_core.pyx'],
         depends=['zipline/lib/adjustment.pxd'],
     ),
-]
+])
 
 
 STR_TO_CMP = {
@@ -247,21 +204,22 @@ def setup_requirements(requirements_path, module_names,
 conda_build = os.path.basename(sys.argv[0]) in ('conda-build',  # unix
                                                 'conda-build-script.py')  # win
 
-setup_requires = setup_requirements(
-    'etc/requirements_build.in',
-    ('Cython', 'numpy'),
-    conda_format=conda_build,
-)
-
-conditional_arguments = {
-    'setup_requires' if not conda_build else 'build_requires': setup_requires,
-}
+if conda_build:
+    conditional_arguments = {
+        'build_requires': setup_requirements(
+            'etc/requirements_build.in',
+            ('Cython', 'numpy'),
+            conda_format=conda_build,
+        )
+    }
+else:
+    conditional_arguments = {}
 
 setup(
     name='zipline',
     url="http://zipline.io",
     version=versioneer.get_version(),
-    cmdclass=LazyBuildExtCommandClass(versioneer.get_cmdclass()),
+    cmdclass=versioneer.get_cmdclass(),
     description='A backtester for financial algorithms.',
     entry_points={
         'console_scripts': [
