13750f95b8c19fe170e19da1bc5990d35d9f52fe
Scott Sanderson <ssanderson@quantopian.com>
2020-03-03 13:34:40 -0500

    TMP: remove spam

--- etc/install-newpandas.sh
+++ etc/install-newpandas.sh
@@ -5,5 +5,5 @@ echo
 echo "Installing zipline using $(which python)"
 echo
 
-pip install -v -r etc/requirements_build.in -c etc/constraints_newpandas.txt
-pip install -v -r .[all] -r etc/requirements_blaze.in -c etc/constraints_newpandas.txt
+pip install -r etc/requirements_build.in -c etc/constraints_newpandas.txt
+pip install -r .[all] -r etc/requirements_blaze.in -c etc/constraints_newpandas.txt
--- etc/install-oldpandas.sh
+++ etc/install-oldpandas.sh
@@ -5,5 +5,5 @@ echo
 echo "Installing zipline using $(which python)"
 echo
 
-python -m pip install -v -r etc/requirements_build.in -c etc/constraints_oldpandas.txt
-python -m pip install -v "$@"
+python -m pip install -r etc/requirements_build.in -c etc/constraints_oldpandas.txt
+python -m pip install "$@"
