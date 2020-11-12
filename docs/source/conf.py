c75e642a0b206d10c0b67323d1c03dff17c8158c
Shlomi Kushchi <shlomik@gmail.com>
2020-11-12 14:03:27 +0200

    docs:: add to path

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,6 +6,7 @@ import os
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 sys.path.insert(0, os.path.abspath('.'))
 sys.path.insert(0, os.path.abspath('..'))
+sys.path.insert(0, os.path.abspath('../../'))
 from zipline import __version__ as version
 
 
