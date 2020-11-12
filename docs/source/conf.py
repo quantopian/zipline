582c03f8b30805bf4457dd813ff313433ad3fd39
Shlomi Kushchi <shlomik@gmail.com>
2020-11-12 12:42:43 +0200

    docs: fix import

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,13 +1,13 @@
 import sys
 import os
 
-from zipline import __version__ as version
-
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 sys.path.insert(0, os.path.abspath('.'))
 sys.path.insert(0, os.path.abspath('..'))
+from zipline import __version__ as version
+
 
 extensions = [
     'sphinx.ext.autodoc',
