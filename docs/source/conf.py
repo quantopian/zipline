3d249281cd3b8e3ab5699356fc1201e86236d5d2
Shlomi Kushchi <shlomik@gmail.com>
2020-11-15 14:57:08 +0200

    docs:: correct version

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -46,7 +46,7 @@ copyright = u'2020, Quantopian Inc.'
 
 # The full version, including alpha/beta/rc tags, but excluding the commit hash
 # version = release = version.split('+', 1)[0]
-version = release = "1.4.0"
+version = release = "1.4.1"
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
