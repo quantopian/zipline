b7bdc53ef5bfb83a7226b5dfbaa2f93792c8c9a1
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-07 08:23:59 +0000

    Adding tarfile member sanitization to extractall()

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -50,7 +50,29 @@ class ExamplesTests(WithTmpDir, ZiplineTestCase):
         cls.add_class_callback(partial(unregister, 'test'))
 
         with tarfile.open(test_resource_path('example_data.tar.gz')) as tar:
-            tar.extractall(cls.tmpdir.path)
+            
+            import os
+            
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(tar, cls.tmpdir.path)
 
         cls.expected_perf = dataframe_cache(
             cls.tmpdir.getpath(
